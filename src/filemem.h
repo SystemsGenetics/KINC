22e18855fa70e0a49a95c14661ab7b4476c3f11b
Josh B <4ctrl.alt.del@gmail.com>
2016-01-30 18:33:13 -0800

    Added filemem interface.
    Added new abstract class that will define how a file memory mapping
    object will behave. Also working on basics of pointer nested class that
    will handle memory objects.

---
+++ src/filemem.h
@@ -0,0 +1,89 @@
+#ifndef FILEMEM_H
+#define FILEMEM_H
+#include <string>
+#include <cstdint>
+#include "exception.h"
+
+
+
+class FileMem
+{
+public:
+   struct Exception;
+   struct SystemError;
+   struct InvalidFile;
+   struct FileSegFault;
+   struct OutOfMemory;
+   template<class T> class Ptr;
+   using VPtr = int64_t;
+   virtual ~FileMem() = default;
+   virtual void clear() = 0;
+   virtual bool reserve(int64_t) = 0;
+   virtual int64_t size() = 0;
+   virtual int64_t available() = 0;
+   constexpr const static char* _identString = "KINCBINDAT";
+protected:
+   virtual void write(void*,VPtr,int) = 0;
+   virtual void read(void*,VPtr,int) = 0;
+   virtual VPtr allocate(int) = 0;
+};
+
+
+
+// in <type_traits> std::is_integral<T>::value &&
+//                  std::is_floating_point<T>::value :D
+// and std::is_class<T>::value AND std::is_polymorphic<T>::value :D
+template<class T> class FileMem::Ptr
+{
+public:
+   Ptr(const Ptr<T>&) = delete;
+   Ptr<T>& operator=(const Ptr<T>&) = delete;
+   Ptr(FileMem&,T,int);
+   Ptr(FileMem&,int);
+   Ptr(FileMem&,VPtr,int);
+   Ptr(Ptr<T>&&);
+   Ptr<T>& operator=(Ptr<T>&&);
+   Ptr<T>& operator=(VPtr);
+   T& operator*();
+   T& operator[](int);
+   VPtr addr();
+   void save();
+   void free();
+};
+
+
+
+struct FileMem::Exception : public ::Exception
+{
+   using ::Exception::Exception;
+};
+
+struct FileMem::SystemError : public ::SystemError
+{
+   using ::SystemError::SystemError;
+};
+
+struct FileMem::InvalidFile : public FileMem::Exception
+{
+   InvalidFile(const char* file, int line):
+      Exception(file,line,"FileMem::InvalidFile")
+   {}
+};
+
+struct FileMem::FileSegFault : public FileMem::Exception
+{
+   FileSegFault(const char* file, int line):
+      Exception(file,line,"FileMem::FileSegFault")
+   {}
+};
+
+struct FileMem::OutOfMemory : public FileMem::Exception
+{
+   OutOfMemory(const char* file, int line):
+      Exception(file,line,"FileMem::OutOfMemory")
+   {}
+};
+
+
+
+#endif
