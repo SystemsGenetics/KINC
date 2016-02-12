ef164c50e40864e385a2d907326f61b32fc36016
Josh B <4ctrl.alt.del@gmail.com>
2016-02-12 12:55:42 -0800

    Added histitem class.
    Added new classs that represents a single history item for the history
    file format of KINC data files. This will make the history class much
    easier to implement.

---
+++ src/histitem.cpp
@@ -0,0 +1,262 @@
+#include "histitem.h"
+
+
+
+HistItem& HistItem::operator=(const HistItem& hist)
+{
+   bool cond = _item.addr()==FileMem::nullPtr;
+   assert<AlreadyAllocated>(cond,__FILE__,__LINE__);
+   _mem.allocate(_item);
+   _fileName = hist.fileName();
+   _object = hist.object();
+   _command = hist.command();
+   _item.timeStamp() = hist.timeStamp();
+   _item.fileNamePtr() = set_string(_fileName);
+   _item.fileNameSize() = _fileName.size()+1;
+   _item.objectPtr() = set_string(_object);
+   _item.objectSize() = _object.size()+1;
+   _item.commandPtr() = set_string(_command);
+   _item.commandSize() = _command.size()+1;
+   _item.childHead() = rec_add_item(hist._mem,hist.childHead());
+   _item.next() = rec_add_item(hist._mem,hist.next());
+   _mem.sync(_item,FileSync::write);
+}
+
+
+
+HistItem::HistItem(FileMem& mem, FileMem::Ptr ptr):
+   _mem(mem),
+   _item(ptr)
+{
+   if (_item.addr()!=FileMem::nullPtr)
+   {
+      load_item();
+   }
+}
+
+
+
+void HistItem::operator=(FileMem::Ptr ptr)
+{
+   _item = ptr;
+   _fileName.clear();
+   _object.clear();
+   _command.clear();
+   if (_item.addr()!=FileMem::nullPtr)
+   {
+      load_item();
+   }
+}
+
+
+
+void HistItem::allocate()
+{
+   bool cond = _item.addr()==FileMem::nullPtr;
+   assert<AlreadyAllocated>(cond,__FILE__,__LINE__);
+   _item.timeStamp() = 0;
+   _item.fileNamePtr() = FileMem::nullPtr;
+   _item.fileNameSize() = 0;
+   _item.objectPtr() = FileMem::nullPtr;
+   _item.objectSize() = 0;
+   _item.commandPtr() = FileMem::nullPtr;
+   _item.commandSize() = 0;
+   _item.childHead() = FileMem::nullPtr;
+   _item.next() = FileMem::nullPtr;
+   _mem.allocate(_item);
+   _mem.sync(_item,FileSync::write);
+}
+
+
+
+void HistItem::sync()
+{
+   bool cond = _item.addr()!=FileMem::nullPtr;
+   assert<NullPtr>(cond,__FILE__,__LINE__);
+   _mem.sync(_item,FileSync::write);
+}
+
+
+
+void HistItem::timeStamp(int64_t n)
+{
+   bool cond = _item.addr()!=FileMem::nullPtr;
+   assert<NullPtr>(cond,__FILE__,__LINE__);
+   _item.timeStamp() = n;
+}
+
+
+
+int64_t HistItem::timeStamp() const
+{
+   bool cond = _item.addr()!=FileMem::nullPtr;
+   assert<NullPtr>(cond,__FILE__,__LINE__);
+   return _item.timeStamp();
+}
+
+
+
+void HistItem::fileName(const std::string& str)
+{
+   bool cond = _item.addr()!=FileMem::nullPtr;
+   assert<NullPtr>(cond,__FILE__,__LINE__);
+   cond = _item.fileNamePtr()==FileMem::nullPtr;
+   assert<AlreadySet>(cond,__FILE__,__LINE__);
+   _fileName = str;
+   _item.fileNamePtr() = set_string(str);
+   _item.fileNameSize() = str.size()+1;
+}
+
+
+
+std::string HistItem::fileName() const
+{
+   bool cond = _item.addr()!=FileMem::nullPtr;
+   assert<NullPtr>(cond,__FILE__,__LINE__);
+   return _fileName;
+}
+
+
+
+void HistItem::object(const std::string& str)
+{
+   bool cond = _item.addr()!=FileMem::nullPtr;
+   assert<NullPtr>(cond,__FILE__,__LINE__);
+   cond = _item.objectPtr()==FileMem::nullPtr;
+   assert<AlreadySet>(cond,__FILE__,__LINE__);
+   _object = str;
+   _item.objectPtr() = set_string(str);
+   _item.objectSize() = str.size()+1;
+}
+
+
+
+std::string HistItem::object() const
+{
+   bool cond = _item.addr()!=FileMem::nullPtr;
+   assert<NullPtr>(cond,__FILE__,__LINE__);
+   return _object;
+}
+
+
+
+void HistItem::command(const std::string& str)
+{
+   bool cond = _item.addr()!=FileMem::nullPtr;
+   assert<NullPtr>(cond,__FILE__,__LINE__);
+   cond = _item.commandPtr()==FileMem::nullPtr;
+   assert<AlreadySet>(cond,__FILE__,__LINE__);
+   _command = str;
+   _item.commandPtr() = set_string(str);
+   _item.commandSize() = str.size()+1;
+}
+
+
+
+std::string HistItem::command() const
+{
+   bool cond = _item.addr()!=FileMem::nullPtr;
+   assert<NullPtr>(cond,__FILE__,__LINE__);
+   return _command;
+}
+
+
+
+void HistItem::next(FileMem::Ptr ptr)
+{
+   bool cond = _item.addr()!=FileMem::nullPtr;
+   assert<NullPtr>(cond,__FILE__,__LINE__);
+   cond = _item.next()==FileMem::nullPtr;
+   assert<AlreadySet>(cond,__FILE__,__LINE__);
+   _item.next() = ptr;
+}
+
+
+
+FileMem::Ptr HistItem::next() const
+{
+   bool cond = _item.addr()!=FileMem::nullPtr;
+   assert<NullPtr>(cond,__FILE__,__LINE__);
+   return _item.next();
+}
+
+
+
+void HistItem::childHead(FileMem::Ptr ptr)
+{
+   bool cond = _item.addr()!=FileMem::nullPtr;
+   assert<NullPtr>(cond,__FILE__,__LINE__);
+   cond = _item.childHead()==FileMem::nullPtr;
+   assert<AlreadySet>(cond,__FILE__,__LINE__);
+   _item.childHead() = ptr;
+}
+
+
+
+FileMem::Ptr HistItem::childHead() const
+{
+   bool cond = _item.addr()!=FileMem::nullPtr;
+   assert<NullPtr>(cond,__FILE__,__LINE__);
+   return _item.childHead();
+}
+
+
+
+FileMem::Ptr HistItem::addr() const
+{
+   return _item.addr();
+}
+
+
+
+void HistItem::load_item()
+{
+   _mem.sync(_item,FileSync::read);
+   if (_item.fileNamePtr()!=FileMem::nullPtr)
+   {
+      _fileName = get_string(_item.fileNamePtr(),_item.fileNameSize());
+   }
+   if (_item.objectPtr()!=FileMem::nullPtr)
+   {
+      _object = get_string(_item.objectPtr(),_item.objectSize());
+   }
+   if (_item.commandPtr()!=FileMem::nullPtr)
+   {
+      _command = get_string(_item.commandPtr(),_item.commandSize());
+   }
+}
+
+
+
+std::string HistItem::get_string(FileMem::Ptr ptr, FileMem::SizeT size)
+{
+   String str(size,ptr);
+   _mem.sync(str,FileSync::read);
+   return {str.c_str()};
+}
+
+
+
+FileMem::Ptr HistItem::set_string(const std::string& newStr)
+{
+   String str(newStr.size()+1);
+   _mem.allocate(str);
+   memcpy(str.c_str(),newStr.c_str(),newStr.size()+1);
+   _mem.sync(str,FileSync::write);
+   return str.addr();
+}
+
+
+
+FileMem::Ptr HistItem::rec_add_item(FileMem& mem, FileMem::Ptr ptr)
+{
+   bool ret = ptr;
+   if (ptr!=FileMem::nullPtr)
+   {
+      HistItem from(mem,ptr);
+      HistItem to(_mem);
+      to = from;
+      ret = to.addr();
+   }
+   return ret;
+}
---
+++ src/histitem.h
@@ -0,0 +1,133 @@
+#ifndef HISTITEM_H
+#define HISTITEM_H
+#include <string>
+#include "filemem.h"
+#include "exception.h"
+
+
+
+namespace HistItemData
+{
+   struct Item;
+   struct String;
+   constexpr static int nodeLen = 60;
+}
+
+struct HistItemData::Item : FileMem::Static<nodeLen>
+{
+   using Static<nodeLen>::Static;
+   int64_t& timeStamp() { get<int64_t>(0); }
+   FileMem::Ptr& fileNamePtr() { get<FileMem::Ptr>(8); }
+   int32_t& fileNameSize() { get<int32_t>(16); }
+   FileMem::Ptr& objectPtr() { get<FileMem::Ptr>(20); }
+   int32_t& objectSize() { get<int32_t>(28); }
+   FileMem::Ptr& commandPtr() { get<FileMem::Ptr>(32); }
+   int32_t& commandSize() { get<int32_t>(40); }
+   FileMem::Ptr& childHead() { get<FileMem::Ptr>(44); }
+   FileMem::Ptr& next() { get<FileMem::Ptr>(52); }
+};
+
+struct HistItemData::String : FileMem::Object
+{
+   using Object::Object;
+   String(FileMem::SizeT size): Object(size) {}
+   char* c_str() { &get<char>(0); }
+};
+
+
+
+class HistItem
+{
+public:
+   // *
+   // * EXCEPTIONS
+   // *
+   struct Exception;
+   struct AlreadySet;
+   struct AlreadyAllocated;
+   struct NullPtr;
+   // *
+   // * BASIC METHODS
+   // *
+   HistItem(HistItem&&) = delete;
+   HistItem& operator=(HistItem&&) = delete;
+   HistItem(const HistItem&) = delete;
+   HistItem& operator=(const HistItem&);
+   HistItem(FileMem&,FileMem::Ptr = FileMem::nullPtr);
+   void operator=(FileMem::Ptr);
+   // *
+   // * FUNCTIONS
+   // *
+   void allocate();
+   void sync();
+   void timeStamp(int64_t);
+   int64_t timeStamp() const;
+   void fileName(const std::string&);
+   std::string fileName() const;
+   void object(const std::string&);
+   std::string object() const;
+   void command(const std::string&);
+   std::string command() const;
+   void next(FileMem::Ptr);
+   FileMem::Ptr next() const;
+   void childHead(FileMem::Ptr);
+   FileMem::Ptr childHead() const;
+   FileMem::Ptr addr() const;
+private:
+   // *
+   // * DECLERATIONS
+   // *
+   using Item = HistItemData::Item;
+   using String = HistItemData::String;
+   // *
+   // * FUNCTIONS
+   // *
+   void load_item();
+   std::string get_string(FileMem::Ptr,FileMem::SizeT);
+   FileMem::Ptr set_string(const std::string&);
+   FileMem::Ptr rec_add_item(FileMem&,FileMem::Ptr);
+   // *
+   // * CONSTANTS
+   // *
+   constexpr static int _nodeLen = HistItemData::nodeLen;
+   // *
+   // * VARIABLES
+   // *
+   FileMem& _mem;
+   mutable Item _item;
+   std::string _fileName;
+   std::string _object;
+   std::string _command;
+};
+
+
+
+struct HistItem::Exception : public ::Exception
+{
+   using ::Exception::Exception;
+};
+
+struct HistItem::AlreadySet : public HistItem::Exception
+{
+   AlreadySet(const char* file, int line):
+      Exception(file,line,"History::AlreadySet")
+   {}
+};
+
+struct HistItem::AlreadyAllocated : public HistItem::Exception
+{
+   AlreadyAllocated(const char* file, int line):
+      Exception(file,line,"History::AlreadyAllocated")
+   {}
+};
+
+struct HistItem::NullPtr : public HistItem::Exception
+{
+   NullPtr(const char* file, int line):
+      Exception(file,line,"History::NullPtr")
+   {}
+};
+
+
+
+#endif
