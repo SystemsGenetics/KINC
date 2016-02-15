7b05bfe7b90a437831962e6bc7d4d116683dfd68
Josh B <4ctrl.alt.del@gmail.com>
2016-02-15 14:54:18 -0800

    Fixed missed file additions.
    Forgot to add additional unit testing files for commit from last commit
    about adding histitem unit testing.

--- src/unit.h
+++ src/unit.h
@@ -33,6 +33,38 @@ namespace unit
       bool sync5();
       bool sync6();
    }
+   namespace histitem
+   {
+      bool main();
+      bool init1();
+      bool allocate1();
+      bool init2();
+      bool allocate2();
+      bool init3();
+      bool init4();
+      bool timestamp1();
+      bool timestamp2();
+      bool filename1();
+      bool filename2();
+      bool filename3();
+      bool filename4();
+      bool object1();
+      bool object2();
+      bool object3();
+      bool object4();
+      bool command1();
+      bool command2();
+      bool command3();
+      bool command4();
+      bool next1();
+      bool next2();
+      bool next3();
+      bool next4();
+      bool childhead1();
+      bool childhead2();
+      bool childhead3();
+      bool childhead4();
+   }
    inline void header(const char*);
    inline void end();
    inline void start();
