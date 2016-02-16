ab4bdde24596fda6d81dd7653627a9fb80d17209
Josh B <4ctrl.alt.del@gmail.com>
2016-02-16 14:21:57 -0800

    Revamped history class.
    Redid history class now that it is a simple wrapper to the histitem
    class which does the heavy lifting. Also redid the iterator class for
    history.

--- src/history.cpp
+++ src/history.cpp
@@ -2,109 +2,65 @@
 
 
 
-History::History(const std::string& fileName):
-   _mem(fileName)
+History::History(FileMem& mem, FileMem::Ptr ptr):
+   _mem(mem),
+   _head(mem,ptr)
 {
-   if (_mem.size()==0)
+   if (ptr==FileMem::nullPtr)
    {
-      _mem.reserve(_emptyLen);
-      _mem.allocate(_header);
-      _mem.allocate(_info);
-      _header.head() = _info.addr();
-      _header.dataHead() = FileMem::nullPtr;
-      _mem.sync(_header,FileSync::write);
-      _info.timeStamp() = 0;
-      _info.fileNamePtr() = FileMem::nullPtr;
-      _info.fileNameSize() = 0;
-      _info.objectPtr() = FileMem::nullPtr;
-      _info.objectSize() = 0;
-      _info.commandPtr() = FileMem::nullPtr;
-      _info.commandSize() = 0;
-      _info.childHead() = FileMem::nullPtr;
-      _info.next() = FileMem::nullPtr;
-      _mem.sync(_info,FileSync::write);
+      _head.allocate();
+      _head.sync();
+   }
+}
+
+
+
+void History::timeStamp(int64_t ts)
+{
+   _head.timeStamp(ts);
+}
+
+
+
+void History::fileName(const std::string& fileName)
+{
+   _head.fileName(fileName);
+}
+
+
+
+void History::object(const std::string& object)
+{
+   _head.object(object);
+}
+
+
+
+void History::command(const std::string& command)
+{
+   _head.command(command);
+}
+
+
+
+void History::add_child(const History& child)
+{
+   HistItem nChild(_mem);
+   nChild = child._head;
+   if (_head.childHead()==FileMem::nullPtr)
+   {
+      _head.childHead(nChild.addr());
+      _head.sync();
    }
    else
    {
-      bool cond = _mem.size()>=_emptyLen;
-      assert<InvalidFile>(cond,__FILE__,__LINE__);
-      _header = _mem.head();
-      _mem.sync(_header,FileSync::read);
-      cond = true;
-      for (int i=0;i<_idLen;++i)
+      HistItem tmp(_mem,_head.childHead());
+      while (tmp.next()!=FileMem::nullPtr)
       {
-         if (_header.ident()[i]!=_identString[i])
-         {
-            cond = false;
-         }
+         tmp = tmp.next();
       }
-      assert<InvalidFile>(cond,__FILE__,__LINE__);
-      _info = _header.head();
-      _mem.sync(_info,FileSync::read);
-   }
-}
-
-
-
-void History::set_timestamp(int64_t tStamp)
-{
-   _info.timeStamp() = tStamp;
-   _mem.sync(_info,FileSync::write);
-}
-
-
-
-void History::set_filename(const std::string& name)
-{
-   bool cond = _info.fileNamePtr()==FileMem::nullPtr;
-   assert<AlreadySet>(cond,__FILE__,__LINE__);
-   _info.fileNamePtr() = write_string(name);
-   _info.fileNameSize() = name.size();
-   _mem.sync(_info,FileSync::write);
-}
-
-
-
-void History::set_object(const std::string& object)
-{
-   bool cond = _info.objectPtr()==FileMem::nullPtr;
-   assert<AlreadySet>(cond,__FILE__,__LINE__);
-   _info.objectPtr() = write_string(object);
-   _info.objectSize() = object.size();
-   _mem.sync(_info,FileSync::write);
-}
-
-
-
-void History::set_command(const std::string& command)
-{
-   bool cond = _info.commandPtr()==FileMem::nullPtr;
-   assert<AlreadySet>(cond,__FILE__,__LINE__);
-   _info.commandPtr() = write_string(command);
-   _info.commandSize() = command.size();
-   _mem.sync(_info,FileSync::write);
-}
-
-
-
-void History::add_child(History& child)
-{
-   if (_info.childHead()==FileMem::nullPtr)
-   {
-      _info.childHead() = rec_add_child(child.begin());
-      _mem.sync(_info,FileSync::write);
-   }
-   else
-   {
-      Node i = _info.childHead();
-      _mem.sync(i,FileSync::read);
-      while (i.next()!=FileMem::nullPtr)
-      {
-         i = i.next();
-         _mem.sync(i,FileSync::read);
-      }
-      i.next() = rec_add_child(child.begin());
-      _mem.sync(i,FileSync::write);
+      tmp.next(nChild.addr());
+      tmp.sync();
    }
 }
 
@@ -112,156 +68,50 @@ void History::add_child(History& child)
 
 History::Iterator History::begin()
 {
-   return {_info.childHead(),_mem};
+   return {_mem,_head.childHead()};
 }
 
 
 
 History::Iterator History::end()
 {
-   return {FileMem::nullPtr,_mem};
-}
-
-
-
-FileMem::Ptr History::write_string(const std::string& str)
-{
-   int rLen = str.size() + 1;
-   if (_mem.capacity()<rLen)
-   {
-      _mem.reserve(rLen - _mem.capacity());
-   }
-   String string(rLen);
-   memcpy(string.c_str(),str.c_str(),rLen);
-   _mem.allocate(string);
-   _mem.sync(string,FileSync::write);
-}
-
-
-
-FileMem::Ptr History::rec_add_child(Iterator i)
-{
-   if (i!=end())
-   {
-      Node child = *i;
-      _mem.allocate(child);
-      copy_child(child,i);
-      child.childHead() = rec_add_child(i.childHead());
-      child.next() = rec_add_child(++i);
-      _mem.sync(child,FileSync::write);
-      return child.addr();
-   }
-   else
-   {
-      return FileMem::nullPtr;
-   }
-}
-
-
-
-void History::copy_child(Node& child, Iterator& i)
-{
-   bool cond = child.fileNamePtr()!=FileMem::nullPtr&&
-               child.objectPtr()!=FileMem::nullPtr&&
-               child.commandPtr()!=FileMem::nullPtr;
-   assert<NotSet>(cond,__FILE__,__LINE__);
-   child.fileNamePtr() = write_string(i.filename());
-   child.objectPtr() = write_string(i.object());
-   child.commandPtr() = write_string(i.command());
-}
-
-
-
-int64_t History::Iterator::timestamp()
-{
-   return _info.timeStamp();
-}
-
-
-
-std::string History::Iterator::filename()
-{
-   std::string ret;
-   if (_ptr!=FileMem::nullPtr&&_info.fileNamePtr()!=FileMem::nullPtr)
-   {
-      String string(_info.fileNameSize(),_info.fileNamePtr());
-      _mem.sync(string,FileSync::read);
-      ret = string.c_str();
-   }
-   return ret;
-}
-
-
-
-std::string History::Iterator::object()
-{
-   std::string ret;
-   if (_ptr!=FileMem::nullPtr&&_info.objectPtr()!=FileMem::nullPtr)
-   {
-      String string(_info.objectSize(),_info.objectPtr());
-      _mem.sync(string,FileSync::read);
-      ret = string.c_str();
-   }
-   return ret;
-}
-
-
-
-std::string History::Iterator::command()
-{
-   std::string ret;
-   if (_ptr!=FileMem::nullPtr&&_info.commandPtr()!=FileMem::nullPtr)
-   {
-      String string(_info.commandSize(),_info.commandPtr());
-      _mem.sync(string,FileSync::read);
-      ret = string.c_str();
-   }
-   return ret;
+   return {_mem};
 }
 
 
 
 History::Iterator History::Iterator::childHead()
 {
-   return {_info.childHead(),_mem};
+   return {_mem,_item.childHead()};
 }
 
 
 
-History::Iterator& History::Iterator::operator++()
+HistItem& History::Iterator::operator*()
 {
-   _ptr = _info.next();
-   if (_ptr!=FileMem::nullPtr)
+   return _item;
+}
+
+
+
+void History::Iterator::operator++()
+{
+   if (_item.addr()!=FileMem::nullPtr)
    {
-      _info = _ptr;
-      _mem.sync(_info,FileSync::read);
+      _item = _item.next();
    }
-   return *this;
 }
 
 
 
 bool History::Iterator::operator!=(const Iterator& cmp)
 {
-   return _ptr!=cmp._ptr;
+   return _item.addr()!=cmp._item.addr();
 }
 
 
 
-History::Iterator::Iterator(FileMem::Ptr ptr, FileMem& mem):
-   _ptr(ptr),
-   _mem(mem)
-{
-   _info.next() = FileMem::nullPtr;
-   if (_ptr!=FileMem::nullPtr)
-   {
-      _mem.sync(_info,FileSync::read);
-   }
-}
-
-
-
-History::Node& History::Iterator::operator*()
-{
-   return _info;
-}
+History::Iterator::Iterator(FileMem& mem, FileMem::Ptr ptr):
+   _mem(mem),
+   _item(mem,ptr)
+{}
--- src/history.h
+++ src/history.h
@@ -1,62 +1,14 @@
 #ifndef HISTORY_H
 #define HISTORY_H
 #include <string>
-#include "filemem.h"
+#include "histitem.h"
 #include "exception.h"
-
-
-
-namespace HistoryData
-{
-   struct Header;
-   struct Node;
-   struct String;
-   constexpr static int _idLen = 9;
-   constexpr static int _nodeLen = 60;
-   constexpr static int _hdrLen = _idLen+16;
-}
-
-struct HistoryData::Header : FileMem::Static<_hdrLen>
-{
-   using Static<_hdrLen>::Static;
-   FileMem::Ptr& head() { get<FileMem::Ptr>(0); }
-   FileMem::Ptr& dataHead() { get<FileMem::Ptr>(8); }
-   char* ident() { &get<char>(16); }
-};
-
-struct HistoryData::Node : FileMem::Static<_nodeLen>
-{
-   using Static<_nodeLen>::Static;
-   int64_t& timeStamp() { get<int64_t>(0); }
-   FileMem::Ptr& fileNamePtr() { get<FileMem::Ptr>(8); }
-   int32_t& fileNameSize() { get<int32_t>(16); }
-   FileMem::Ptr& objectPtr() { get<FileMem::Ptr>(20); }
-   int32_t& objectSize() { get<int32_t>(28); }
-   FileMem::Ptr& commandPtr() { get<FileMem::Ptr>(32); }
-   int32_t& commandSize() { get<int32_t>(40); }
-   FileMem::Ptr& childHead() { get<FileMem::Ptr>(44); }
-   FileMem::Ptr& next() { get<FileMem::Ptr>(52); }
-};
-
-struct HistoryData::String : FileMem::Object
-{
-   using Object::Object;
-   String(FileMem::SizeT size): Object(size) {}
-   char* c_str() { &get<char>(0); }
-};
 
 
 
 class History
 {
 public:
-   // *
-   // * EXCEPTIONS
-   // *
-   struct Exception;
-   struct InvalidFile;
-   struct AlreadySet;
-   struct NotSet;
    // *
    // * DECLERATIONS
    // *
@@ -68,45 +20,23 @@ public:
    History(History&&) = delete;
    History& operator=(const History&) = delete;
    History& operator=(History&&) = delete;
-   History(const std::string&);
+   History(FileMem&,FileMem::Ptr = FileMem::nullPtr);
    // *
    // * FUNCTIONS
    // *
-   void set_timestamp(int64_t);
-   void set_filename(const std::string&);
-   void set_object(const std::string&);
-   void set_command(const std::string&);
-   void add_child(History&);
+   void timeStamp(int64_t);
+   void fileName(const std::string&);
+   void object(const std::string&);
+   void command(const std::string&);
+   void add_child(const History&);
    Iterator begin();
    Iterator end();
 private:
    // *
-   // * DECLERATIONS
-   // *
-   using Header = HistoryData::Header;
-   using Node = HistoryData::Node;
-   using String = HistoryData::String;
-   // *
-   // * FUNCTIONS
-   // *
-   FileMem::Ptr write_string(const std::string&);
-   FileMem::Ptr rec_add_child(Iterator);
-   void copy_child(Node&,Iterator&);
-   // *
-   // * CONSTANTS
-   // *
-   constexpr static int _idLen = HistoryData::_idLen;
-   constexpr static int _nodeLen = HistoryData::_nodeLen;
-   constexpr static int _hdrLen = HistoryData::_hdrLen;
-   constexpr static int _emptyLen = _hdrLen + _nodeLen;
-   constexpr const static char* _identString = "\113\111\116\103\102\106\111"
-                                               "\114\105";
-   // *
    // * VARIABLES
    // *
-   FileMem _mem;
-   Header _header;
-   Node _info;
+   FileMem& _mem;
+   HistItem _head;
 };
 
 
@@ -121,59 +51,25 @@ public:
    // *
    // * FUNCTIONS
    // *
-   int64_t timestamp();
-   std::string filename();
-   std::string object();
-   std::string command();
    Iterator childHead();
    // *
    // * OPERATORS
    // *
-   Iterator& operator++();
-   bool operator!=(const Iterator&); ///TODO
+   HistItem& operator*();
+   void operator++();
+   bool operator!=(const Iterator&);
 private:
    // *
    // * BASIC METHODS
    // *
-   Iterator(FileMem::Ptr,FileMem&);
-   // *
-   // * OPERATORS
-   // *
-   Node& operator*();
+   Iterator(FileMem&,FileMem::Ptr = FileMem::nullPtr);
    // *
    // * VARIABLES
    // *
    FileMem& _mem;
-   FileMem::Ptr _ptr;
-   Node _info;
+   HistItem _item;
 };
 
 
-
-struct History::Exception : public ::Exception
-{
-   using ::Exception::Exception;
-};
-
-struct History::InvalidFile : public History::Exception
-{
-   InvalidFile(const char* file, int line):
-      Exception(file,line,"History::InvalidFile")
-   {}
-};
-
-struct History::AlreadySet : public History::Exception
-{
-   AlreadySet(const char* file, int line):
-      Exception(file,line,"History::AlreadySet")
-   {}
-};
-
-struct History::NotSet : public History::Exception
-{
-   NotSet(const char* file, int line):
-      Exception(file,line,"History::NotSet")
-   {}
-};
 
 #endif
