c6be2a1a762e1202c6e410320594d75e0e0bc7f2
Josh B <4ctrl.alt.del@gmail.com>
2016-02-25 10:23:58 -0800

    Updated plugin interfaces.
    Changed the virtual function interface for both plugins to be used in
    the correct fashion.

--- src/analytic.h
+++ src/analytic.h
@@ -14,8 +14,7 @@ public:
    virtual ~Analytic() = default;
    virtual void input(DataPlugin*) = 0;
    virtual void output(DataPlugin*) = 0;
-   virtual void option(const std::string&,const std::string&) = 0;
-   virtual void execute(Terminal&,cl::Device*) = 0;
+   virtual void execute(GetOpts&,Terminal&,cl::Device*) = 0;
 };
 
 
