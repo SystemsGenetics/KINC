9eff93ba0d48807e28e5a7923db8e4e4e4874359
Josh B <4ctrl.alt.del@gmail.com>
2016-03-01 12:55:10 -0800

    Modified history class.
    Made changes to history class necessary for reworked histitem class that
    it depends on. Also made cosmetic changes to header.

--- src/history.cpp
+++ src/history.cpp
@@ -2,59 +2,11 @@
 
 
 
-History::History(FileMem& mem, FileMem::Ptr ptr):
-   _mem(mem),
-   _head(mem,ptr)
-{
-   if (ptr==FileMem::nullPtr)
-   {
-      _head.allocate();
-      _head.sync();
-   }
-}
-
-
-
-FileMem::Ptr History::addr()
-{
-   return _head.addr();
-}
-
-
-
-void History::timeStamp(int64_t ts)
-{
-   _head.timeStamp(ts);
-}
-
-
-
-void History::fileName(const std::string& fileName)
-{
-   _head.fileName(fileName);
-}
-
-
-
-void History::object(const std::string& object)
-{
-   _head.object(object);
-}
-
-
-
-void History::command(const std::string& command)
-{
-   _head.command(command);
-}
-
-
-
 void History::add_child(const History& child)
 {
    HistItem nChild(_mem);
-   nChild = child._head;
-   if (_head.childHead()==FileMem::nullPtr)
+   nChild.copy_from(child._head);
+   if (_head.childHead()==fNullPtr)
    {
       _head.childHead(nChild.addr());
       _head.sync();
@@ -62,7 +14,7 @@ void History::add_child(const History& child)
    else
    {
       HistItem tmp(_mem,_head.childHead());
-      while (tmp.next()!=FileMem::nullPtr)
+      while (tmp.next()!=fNullPtr)
       {
          tmp = tmp.next();
       }
@@ -70,55 +22,3 @@ void History::add_child(const History& child)
       tmp.sync();
    }
 }
-
-
-
-History::Iterator History::begin()
-{
-   return {_mem,_head.childHead()};
-}
-
-
-
-History::Iterator History::end()
-{
-   return {_mem};
-}
-
-
-
-History::Iterator History::Iterator::childHead()
-{
-   return {_mem,_item.childHead()};
-}
-
-
-
-HistItem& History::Iterator::operator*()
-{
-   return _item;
-}
-
-
-
-void History::Iterator::operator++()
-{
-   if (_item.addr()!=FileMem::nullPtr)
-   {
-      _item = _item.next();
-   }
-}
-
-
-
-bool History::Iterator::operator!=(const Iterator& cmp)
-{
-   return _item.addr()!=cmp._item.addr();
-}
-
-
-
-History::Iterator::Iterator(FileMem& mem, FileMem::Ptr ptr):
-   _mem(mem),
-   _item(mem,ptr)
-{}
--- src/history.h
+++ src/history.h
@@ -13,25 +13,30 @@ public:
    // * DECLERATIONS
    // *
    class Iterator;
+   using FPtr = FileMem::Ptr;
    // *
    // * BASIC METHODS
    // *
    History(const History&) = delete;
    History(History&&) = delete;
-   History& operator=(const History&) = delete;
-   History& operator=(History&&) = delete;
-   History(FileMem&,FileMem::Ptr = FileMem::nullPtr);
+   History(FileMem&,FPtr = fNullPtr);
    // *
    // * FUNCTIONS
    // *
-   FileMem::Ptr addr();
-   void timeStamp(int64_t);
-   void fileName(const std::string&);
-   void object(const std::string&);
-   void command(const std::string&);
+   inline FileMem::Ptr addr();
    void add_child(const History&);
-   Iterator begin();
-   Iterator end();
+   inline HistItem& head();
+   inline Iterator begin();
+   inline Iterator end();
+   // *
+   // * OPERATORS
+   // *
+   History& operator=(const History&) = delete;
+   History& operator=(History&&) = delete;
+   // *
+   // * CONSTANTS
+   // *
+   const static auto fNullPtr = FileMem::nullPtr;
 private:
    // *
    // * VARIABLES
@@ -49,27 +54,115 @@ public:
    // * DECLERATIONS
    // *
    friend class History;
+   using FPtr = History::FPtr;
    // *
    // * FUNCTIONS
    // *
-   Iterator childHead();
+   inline Iterator childHead();
    // *
    // * OPERATORS
    // *
-   HistItem& operator*();
-   void operator++();
-   bool operator!=(const Iterator&);
+   inline HistItem& operator*();
+   inline HistItem* operator->();
+   inline void operator++();
+   inline bool operator!=(const Iterator&);
 private:
    // *
    // * BASIC METHODS
    // *
-   Iterator(FileMem&,FileMem::Ptr = FileMem::nullPtr);
+   inline Iterator(FileMem&,FPtr = fNullPtr);
    // *
    // * VARIABLES
    // *
    FileMem& _mem;
    HistItem _item;
 };
+
+
+
+inline History::History(FileMem& mem, FPtr ptr):
+   _mem(mem),
+   _head(mem,ptr)
+{
+   if (ptr==fNullPtr)
+   {
+      _head.allocate();
+      _head.sync();
+   }
+}
+
+
+
+inline FileMem::Ptr History::addr()
+{
+   return _head.addr();
+}
+
+
+
+inline HistItem& History::head()
+{
+   return _head;
+}
+
+
+
+inline History::Iterator History::begin()
+{
+   return {_mem,_head.childHead()};
+}
+
+
+
+inline History::Iterator History::end()
+{
+   return {_mem};
+}
+
+
+
+inline History::Iterator History::Iterator::childHead()
+{
+   return {_mem,_item.childHead()};
+}
+
+
+
+inline HistItem& History::Iterator::operator*()
+{
+   return _item;
+}
+
+
+
+inline HistItem* History::Iterator::operator->()
+{
+   return &_item;
+}
+
+
+
+inline void History::Iterator::operator++()
+{
+   if (_item.addr()!=FileMem::nullPtr)
+   {
+      _item = _item.next();
+   }
+}
+
+
+
+inline bool History::Iterator::operator!=(const Iterator& cmp)
+{
+   return _item.addr()!=cmp._item.addr();
+}
+
+
+
+inline History::Iterator::Iterator(FileMem& mem, FPtr ptr):
+   _mem(mem),
+   _item(mem,ptr)
+{}
 
 
 
