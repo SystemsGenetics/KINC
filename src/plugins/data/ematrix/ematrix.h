d1743d09297dd113c68001600d20f4c1099b2586
Josh B <4ctrl.alt.del@gmail.com>
2016-04-12 13:59:04 -0700

    Added filemem data defintions for ematrix data plugin.

--- src/plugins/data/ematrix/ematrix.h
+++ src/plugins/data/ematrix/ematrix.h
@@ -4,9 +4,51 @@
 
 
 
+namespace ematrixData {
+
+struct Header : public FileMem::Static<33>
+{
+   using FPtr = FileMem::Ptr;
+   using FileMem::Static<33>::Static;
+   uint32_t& geneSize() { get<uint32_t>(0); }
+   uint32_t& sampleSize() { get<uint32_t>(4); }
+   uint8_t& transform() { get<uint8_t>(8); }
+   FPtr& geneSize() { get<FPtr>(9); }
+   FPtr& geneSize() { get<FPtr>(17); }
+   FPtr& geneSize() { get<FPtr>(25); }
+};
+
+struct SampleHdr : public FileMem::Static<8>
+{
+   using FPtr = FileMem::Ptr;
+   using FileMem::Static<8>::Static;
+   FPtr& name() { get<FPtr>(0); }
+};
+
+struct GeneHdr : public FileMem::Static<8>
+{
+   using FPtr = FileMem::Ptr;
+   using FileMem::Static<8>::Static;
+   FPtr& name() { get<FPtr>(0); }
+};
+
+struct Expression : public FileMem::Static<4>
+{
+   using FileMem::Static<4>::Static;
+   float& val() { get<float>(0); }
+};
+
+} // END namespace ematrixData
+
+
+
 class ematrix : public DataPlugin
 {
 public:
+   using Hdr = ematrixData::Header;
+   using gHdr = ematrixData::GeneHdr;
+   using sHdr = ematrixData::SampleHdr;
+   using Exp = ematrixData::Expression;
    using string = std::string;
    ematrix(const string& type, const string& file): DataPlugin(type,file) {}
    void load(GetOpts &ops, Terminal &tm) override final {}
