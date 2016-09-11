1e876c5f8d1474f0f868d89413ed61bbb30eeee4
Josh B <4ctrl.alt.del@gmail.com>
2016-09-11 12:56:59 -0700

    Finished completely refactoring the entire correlation matrix plugin. Nothing has been unit tested but it correctly compiles at least.

--- src/cmatrix.h
+++ src/cmatrix.h
@@ -2,74 +2,116 @@
 #define CMATRIX_H
 #include <ace.h>
 #include <vector>
+#include "ematrix.h"
 
 
-/*
-class CMatrix : public AccelCompEng::DataPlugin
+
+namespace Ace = AccelCompEng;
+
+
+
+class CMatrix : public Ace::Data, private Ace::NVMemory::Node
 {
 public:
-   using string = std::string;
-   ACE_DATA_HEADER()
-   ACE_EXCEPTION(CMatrix,AlreadyInitialized)
-   ACE_EXCEPTION(CMatrix,OutOfRange)
-   ACE_EXCEPTION(CMatrix,NotInitialized)
-   ACE_EXCEPTION(CMatrix,GreaterThanMax)
-   ACE_EXCEPTION(CMatrix,AlreadySet)
+   struct NullMatrix : public Ace::Exception { using Ace::Exception::Exception; };
+   struct OutOfRange : public Ace::Exception { using Ace::Exception::Exception; };
+   struct NotInitialized : public Ace::Exception { using Ace::Exception::Exception; };
+   struct GreaterThanMax : public Ace::Exception { using Ace::Exception::Exception; };
+   struct AlreadySet : public Ace::Exception { using Ace::Exception::Exception; };
+   struct InvalidSize : public Ace::Exception { using Ace::Exception::Exception; };
    class GPair;
-   CMatrix(const string& type, const string& file);
-   void load(GetOpts&,Terminal&) override final;
-   void dump(GetOpts&,Terminal&) override final;
-   void query(GetOpts&,Terminal&) override final;
+   CMatrix();
+   ~CMatrix();
+   void init() override final;
+   void load(Ace::GetOpts&,Ace::Terminal&) override final;
+   void dump(Ace::GetOpts&,Ace::Terminal&) override final;
+   void query(Ace::GetOpts&,Ace::Terminal&) override final;
    bool empty() override final;
    static long long diagonal(int,int);
    static long long diag_size(int);
-   void initialize(int,int,int,int,bool);
-   int gSize() const;
-   int sSize() const;
-   int mSize() const;
-   int cSize() const;
-   string& gName(int);
-   string& sName(int);
-   string& cName(int);
-   void write();
+   void initialize(std::vector<std::string>&& geneNames, std::vector<std::string>&& sampleNames,
+                   std::vector<std::string>&& correlationNames, int maxModes);
+   int gene_size() const;
+   int sample_size() const;
+   int max_modes() const;
+   int correlation_size() const;
+   const std::string& gene_name(int) const;
+   const std::string& sample_name(int) const;
+   const std::string& correlation_name(int) const;
    GPair begin();
    GPair end();
    GPair& at(int,int);
    GPair& ref(int,int);
 private:
-   using svec = std::vector<string>;
-   ACE_FMEM_STATIC(Header,46)
-      ACE_FMEM_VAL(gSize,int32_t,0)
-      ACE_FMEM_VAL(sSize,int32_t,4)
-      ACE_FMEM_VAL(cSize,int32_t,8)
-      ACE_FMEM_VAL(mSize,int8_t,12)
-      ACE_FMEM_VAL(wr,int8_t,13)
-      ACE_FMEM_VAL(gPtr,FPtr,14)
-      ACE_FMEM_VAL(sPtr,FPtr,22)
-      ACE_FMEM_VAL(cPtr,FPtr,30)
-      ACE_FMEM_VAL(eData,FPtr,38)
-   ACE_FMEM_END()
-   ACE_FMEM_STATIC(NmHead,8)
-      ACE_FMEM_VAL(nPtr,FPtr,0)
-   ACE_FMEM_END()
-   string& get_name(int,svec&,FPtr,int);
-   void write_names(svec&,FPtr);
-   Header _hdr;
+   struct __attribute__ ((__packed__)) Header
+   {
+      int32_t _geneSize;
+      int32_t _sampleSize;
+      int32_t _correlationSize;
+      int8_t _maxModes;
+      int64_t _genePtr;
+      int64_t _samplePtr;
+      int64_t _correlationPtr;
+      int64_t _expData;
+   };
+   Header& data() { return get<Header>(); }
+   const Header& data() const { return get<Header>(); }
+   void null_data() override final;
+   void flip_endian() override final;
+   bool _isNew {true};
    GPair* _iGPair {nullptr};
-   svec _gNames;
-   svec _sNames;
-   svec _cNames;
+   std::vector<std::string> _geneNames;
+   std::vector<std::string> _sampleNames;
+   std::vector<std::string> _correlationNames;
+   constexpr static int _strSize {1024};
 };
 
 
 
-class CMatrix::GPair
+class CMatrix::GPair: private Ace::NVMemory::Node
 {
 public:
    friend class CMatrix;
-   class Modes;
-   class Corrs;
-   ~GPair();
+   class Modes
+   {
+   public:
+      friend class GPair;
+      class Mode;
+      Modes(const Modes&);
+      Modes(Modes&&) = default;
+      Modes& operator=(const Modes&);
+      Modes& operator=(Modes&&) = default;
+      Mode begin();
+      Mode end();
+      Mode& at(int);
+      Mode& operator[](int);
+   private:
+      Modes(GPair*);
+      GPair* _p;
+      std::unique_ptr<Mode> _mode {nullptr};
+   };
+   class Corrs
+   {
+   public:
+      friend class GPair;
+      class Corr;
+      Corrs(const Corrs&);
+      Corrs(Corrs&&) = default;
+      Corrs& operator=(const Corrs&);
+      Corrs& operator=(Corrs&&) = default;
+      Corr begin();
+      Corr end();
+      Corr& at(int);
+      Corr& operator[](int);
+   private:
+      Corrs(GPair*);
+      GPair* _p;
+      std::unique_ptr<Corr> _corr {nullptr};
+   };
+   GPair(const GPair&) = default;
+   GPair(GPair&&) = default;
+   GPair& operator=(const GPair&) = default;
+   GPair& operator=(GPair&&) = default;
    void read();
    void write();
    int x() const;
@@ -83,49 +125,18 @@ public:
 private:
    GPair(CMatrix*,int,int);
    void set(int,int);
-   static FPtr initialize(FileMem&,int,int,int,int);
-   ACE_FMEM_OBJECT(Pair)
-      Pair(int mSize, int sSize, int cSize, FPtr ptr = fNullPtr):
-         Object(1+(mSize*sSize)+(4*cSize*mSize),ptr),
-         _sSize(sSize),
-         _cSize(cSize),
-         _indent(1+(mSize*sSize))
-      {}
-      ACE_FMEM_VAL(mAmt,int8_t,0)
-      int8_t& mVal(int mi, int i) { get<int8_t>(1+(_sSize*mi)+i); }
-      float& cVal(int mi, int ci) { get<float>(_indent+(4*((_cSize*mi)+ci))); }
-      int _sSize;
-      int _cSize;
-      int _indent;
-   ACE_FMEM_END()
+   int8_t& mode_val(int mi, int i);
+   float& correlation_val(int mi, int ci);
+   static int64_t initialize(Ace::NVMemory& mem, int geneSize, int maxModes, int sampleSize,
+                             int correlationSize);
+   static size_t calc_size(int maxModes, int sampleSize, int correlationSize);
+   void null_data() override final {}
+   void flip_endian() override final;
    CMatrix* _p;
-   Pair _data;
    int _x;
    int _y;
-   Modes* _iModes {nullptr};
-   Corrs* _iCorrs {nullptr};
-};
-
-
-
-class CMatrix::GPair::Modes
-{
-public:
-   friend class GPair;
-   class Mode;
-   ~Modes();
-   Modes(const Modes&) = delete;
-   Modes& operator=(const Modes&) = delete;
-   Modes(Modes&&) = delete;
-   Modes& operator=(Modes&&) = delete;
-   Mode begin();
-   Mode end();
-   Mode& at(int);
-   Mode& operator[](int);
-private:
-   Modes(GPair*);
-   GPair* _p;
-   Mode* _iMode {nullptr};
+   Modes _modes;
+   Corrs _corrs;
 };
 
 
@@ -165,28 +176,6 @@ private:
 
 
 
-class CMatrix::GPair::Corrs
-{
-public:
-   friend class GPair;
-   class Corr;
-   ~Corrs();
-   Corrs(const Corrs&) = delete;
-   Corrs& operator=(const Corrs&) = delete;
-   Corrs(Corrs&&) = delete;
-   Corrs& operator=(Corrs&&) = delete;
-   Corr begin();
-   Corr end();
-   Corr& at(int);
-   Corr& operator[](int);
-private:
-   Corrs(GPair*);
-   GPair* _p;
-   Corr* _iCorr {nullptr};
-};
-
-
-
 class CMatrix::GPair::Corrs::Corr
 {
 public:
@@ -220,6 +209,6 @@ private:
    int _i;
 };
 
-*/
+
 
 #endif
