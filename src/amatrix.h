5099af91748bdf091cab25792c73176c9effc430
Josh B <4ctrl.alt.del@gmail.com>
2016-09-12 10:17:20 -0700

    Added to amatrix.
    Finished with initial coding for amatrix data plugin, which brought up
    some issues with cmatrix.

--- src/amatrix.h
+++ src/amatrix.h
@@ -11,6 +11,9 @@ namespace Ace = AccelCompEng;
 class AMatrix : public Ace::Data, private Ace::NVMemory::Node
 {
 public:
+   struct AlreadySet : public Ace::Exception { using Ace::Exception::Exception; };
+   struct NullMatrix : public Ace::Exception { using Ace::Exception::Exception; };
+   struct OutOfRange : public Ace::Exception { using Ace::Exception::Exception; };
    class Iterator;
    AMatrix();
    void init() override final;
@@ -27,11 +30,15 @@ private:
    struct __attribute__ ((__packed__)) Header
    {
       int32_t _geneSize {0};
+      int64_t _genePtr {fnullptr};
       int64_t _netData {fnullptr};
    };
+   Header& data() { return get<Header>(); }
+   const Header& data() const { return get<Header>(); }
    void null_data() override final {}
    void flip_endian() override final;
    bool _isNew {true};
+   std::vector<std::string> _geneNames;
    std::unique_ptr<Iterator> _iterator {nullptr};
 };
 
@@ -42,15 +49,20 @@ class AMatrix::Iterator : private Ace::NVMemory::Node
 public:
    void read();
    void write();
-   bool operator*();
+   int x() const;
+   int y() const;
+   bool& operator*();
    bool operator!=(const Iterator&);
    void operator++();
 private:
-   Iterator(AMatrix* p, int i);
+   Iterator(AMatrix* p, int x, int y);
+   void set(int x, int y);
+   static int64_t initialize(Ace::NVMemory& mem, int geneSize);
    void null_data() override final {}
    void flip_endian() override final {}
    AMatrix* _p;
-   int _i;
+   int _x;
+   int _y;
 };
 
 
