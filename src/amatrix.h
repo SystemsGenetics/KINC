45f70f8973824513a6f80cfba449c477e9b78663
Josh B <4ctrl.alt.del@gmail.com>
2016-09-12 07:52:21 -0700

    Added new class.
    Added new amatrix data plugin class as empty template. This plugin will
    store information about a gene network.

---
+++ src/amatrix.h
@@ -0,0 +1,25 @@
+#ifndef AMATRIX_H
+#define AMATRIX_H
+#include <ace.h>
+
+
+
+namespace Ace = AccelCompEng;
+
+
+
+class AMatrix : public Ace::Data, private Ace::NVMemory::Node
+{
+   class Iterator;
+};
+
+
+
+class AMatrix::Iterator
+{
+   ;
+};
+
+
+
+#endif
