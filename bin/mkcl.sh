6f61e38a55cbf2f2be318d008d55529f2fb60532
Josh B <4ctrl.alt.del@gmail.com>
2016-09-15 10:42:16 -0700

    Removed deprecated files.
    Deleted all files related to KINC version 1, now it is KINC version 2.

--- bin/mkcl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "const char* `echo $1 | sed 's/\./_/g'` = " > ${1}.h
-cat $1 | sed 's/$/\\n"/g' | sed 's/^/"/g' >> ${1}.h
-echo '"";' >> ${1}.h
--- stats/kurtosis.h
+++
@@ -1,8 +0,0 @@
-#ifndef _KURTOSIS_
-#define _KURTOSIS_
-
-#include <math.h>
-
-double kurtosis(double *x, int n);
-
-#endif
