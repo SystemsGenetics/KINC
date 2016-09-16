1454c42c8b9363330fb3dd5d226a628490d1fb8e
Josh B <4ctrl.alt.del@gmail.com>
2016-09-15 22:00:11 -0700

    Added to plugin factory.
    Added the spearman analytic plugin to the ACE factory so it can be unit
    tested.

--- src/linker.cpp
+++ src/linker.cpp
@@ -7,7 +7,14 @@
 
 class KINCFactory : public Ace::Factory
 {
-   Ace::Analytic* build_analytic(const std::string&) override final { return nullptr; }
+   Ace::Analytic* build_analytic(const std::string& type) override final {
+      Ace::Analytic* ret {nullptr};
+      if (type==std::string("spearman"))
+      {
+         ret = new Spearman;
+      }
+      return ret;
+   }
    Ace::Data* build_data(const std::string& type) override final
    {
       Ace::Data* ret {nullptr};
