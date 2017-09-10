058b3f2e332c7d207753aa97b4fb1602d0cc728a
Josh B <4ctrl.alt.del@gmail.com>
2017-09-10 11:33:42 -0700

    Finished coding expression matrix.

--- qt/src/expressionmatrix.cpp
+++ qt/src/expressionmatrix.cpp
@@ -11,14 +11,23 @@
 
 void ExpressionMatrix::readData()
 {
+   // seek to beginning to data
    if ( !seek(0) )
    {
-      ;//ERROR
+      E_MAKE_EXCEPTION(e);
+      e.setTitle(tr("File IO Error"));
+      e.setDetails(tr("Failed calling seek() on data object file."));
+      throw e;
    }
+
+   // read header
    stream() >> _geneSize >> _sampleSize;
    if ( !stream() )
    {
-      ;//ERRPOR
+      E_MAKE_EXCEPTION(e);
+      e.setTitle(tr("File IO Error"));
+      e.setDetails(tr("Failed reading from data object file."));
+      throw e;
    }
 }
 
@@ -29,9 +38,10 @@ void ExpressionMatrix::readData()
 
 quint64 ExpressionMatrix::getDataEnd() const
 {
+   // calculate and return end of data
    qint64 geneSize {_geneSize};
    qint64 sampleSize {_sampleSize};
-   return DATA_OFFSET+(geneSize*sampleSize);
+   return DATA_OFFSET+(geneSize*sampleSize*sizeof(float));
 }
 
 
@@ -41,14 +51,23 @@ quint64 ExpressionMatrix::getDataEnd() const
 
 void ExpressionMatrix::newData()
 {
+   // seek to beginning of data
    if ( !seek(0) )
    {
-      ;//ERROR
+      E_MAKE_EXCEPTION(e);
+      e.setTitle(tr("File IO Error"));
+      e.setDetails(tr("Failed calling seek() on data object file."));
+      throw e;
    }
+
+   // write gene and sample sizes of 0
    stream() << _geneSize << _sampleSize;
    if ( !stream() )
    {
-      ;//ERRPOR
+      E_MAKE_EXCEPTION(e);
+      e.setTitle(tr("File IO Error"));
+      e.setDetails(tr("Failed writing to data object file."));
+      throw e;
    }
 }
 
@@ -59,15 +78,25 @@ void ExpressionMatrix::newData()
 
 void ExpressionMatrix::prepare(bool preAllocate)
 {
+   // check if pre-allocation is requested
    if ( preAllocate )
    {
+      // seek to beginning of data
       if ( !seek(0) )
       {
-         ;//ERROR
+         E_MAKE_EXCEPTION(e);
+         e.setTitle(tr("File IO Error"));
+         e.setDetails(tr("Failed calling seek() on data object file."));
+         throw e;
       }
+
+      // allocate total size needed for data
       if ( !allocate(DATA_OFFSET+(_geneSize*_sampleSize*sizeof(float))) )
       {
-         ;//ERROR
+         E_MAKE_EXCEPTION(e);
+         e.setTitle(tr("File IO Error"));
+         e.setDetails(tr("Failed allocating space in data object file."));
+         throw e;
       }
    }
 }
@@ -79,14 +108,23 @@ void ExpressionMatrix::prepare(bool preAllocate)
 
 void ExpressionMatrix::finish()
 {
+   // seek to beginning of data
    if ( !seek(0) )
    {
-      ;//ERROR
+      E_MAKE_EXCEPTION(e);
+      e.setTitle(tr("File IO Error"));
+      e.setDetails(tr("Failed calling seek() on data object file."));
+      throw e;
    }
+
+   // write header information with new sizes
    stream() << _geneSize << _sampleSize;
    if ( !stream() )
    {
-      ;//ERRPOR
+      E_MAKE_EXCEPTION(e);
+      e.setTitle(tr("File IO Error"));
+      e.setDetails(tr("Failed writing to data object file."));
+      throw e;
    }
 }
 
@@ -97,7 +135,10 @@ void ExpressionMatrix::finish()
 
 void ExpressionMatrix::initialize(QStringList geneNames, QStringList sampleNames)
 {
+   // get metadata root of data object
    EMetadata::Map* map {meta().toObject()};
+
+   // create new metadata array that stores gene names and populate it
    EMetadata* metaGeneNames {new EMetadata(EMetadata::Array)};
    for (auto i = geneNames.constBegin(); i != geneNames.constEnd() ;++i)
    {
@@ -105,6 +146,8 @@ void ExpressionMatrix::initialize(QStringList geneNames, QStringList sampleNames
       *(name->toString()) = *i;
       metaGeneNames->toArray()->append(name);
    }
+
+   // create new metadata array that stores sample names and populate it
    EMetadata* metaSampleNames {new EMetadata(EMetadata::Array)};
    for (auto i = sampleNames.constBegin(); i != sampleNames.constEnd() ;++i)
    {
@@ -112,8 +155,12 @@ void ExpressionMatrix::initialize(QStringList geneNames, QStringList sampleNames
       *(name->toString()) = *i;
       metaSampleNames->toArray()->append(name);
    }
+
+   // insert both gene and sample names to data object's metadata
    map->insert("genes",metaGeneNames);
    map->insert("samples",metaSampleNames);
+
+   // set gene and sample size from size of name lists
    _geneSize = geneNames.size();
    _sampleSize = sampleNames.size();
 }
@@ -123,30 +170,133 @@ void ExpressionMatrix::initialize(QStringList geneNames, QStringList sampleNames
 
 
 
+void ExpressionMatrix::setTransform(ExpressionMatrix::Transform transform)
+{
+   // get metadata root of data object and create new string
+   EMetadata::Map* map {meta().toObject()};
+   EMetadata* meta {new EMetadata(EMetadata::String)};
+
+   // set new transform string according to transform type
+   switch (transform)
+   {
+   case Transform::None:
+      *(meta->toString()) = tr("none");
+      break;
+   case Transform::NLog:
+      *(meta->toString()) = tr("natural logarithm");
+      break;
+   case Transform::Log2:
+      *(meta->toString()) = tr("logarithm base 2");
+      break;
+   case Transform::Log10:
+      *(meta->toString()) = tr("logarithm base 10");
+      break;
+   }
+
+   // if transform key already exists in metadata remove it
+   if ( map->contains("transform") )
+   {
+      delete map->take("transform");
+   }
+
+   // add new transform key to metadata
+   map->insert("transform",meta);
+}
+
+
+
+
+
+
+qint64 ExpressionMatrix::getRawSize() const
+{
+   // calculate total number of floating point epxressions and return it
+   qint64 geneSize {_geneSize};
+   qint64 sampleSize {_sampleSize};
+   return geneSize*sampleSize;
+}
+
+
+
+
+
+
+float* ExpressionMatrix::dumpRawData() const
+{
+   // if there are no genes do nothing
+   if ( _geneSize == 0 )
+   {
+      return nullptr;
+   }
+
+   // create new floating point array and populate with all gene expressions
+   float* ret {new float[getRawSize()]};
+   for (int i = 0; i < _geneSize ;++i)
+   {
+      readGene(i,&ret[i*_sampleSize]);
+   }
+
+   // return new float array
+   return ret;
+}
+
+
+
+
+
+
 QVariant ExpressionMatrix::headerData(int section, Qt::Orientation orientation, int role) const
 {
+   // if this is not display role return nothing
    if ( role != Qt::DisplayRole )
    {
       return QVariant();
    }
+
+   // get metadata root and figure out orientation
    const EMetadata::Map* map {meta().toObject()};
    switch (orientation)
    {
    case Qt::Vertical:
+      // make sure metadata contains gene names
       if ( map->contains("genes") )
       {
+         // get gene names and make sure it is array
          EMetadata* genes {(*map)["genes"]};
          if ( genes->isArray() )
          {
+            // make sure section is within limits of array
             if ( section >= 0 && section < genes->toArray()->size() )
             {
+               // return gene name
                return genes->toArray()->at(section)->toVariant();
             }
          }
       }
+
+      // if no gene name found return nothing
+      return QVariant();
    case Qt::Horizontal:
-      break;
+      // make sure metadata contains sample names
+      if ( map->contains("samples") )
+      {
+         // get sample names and make sure it is array
+         EMetadata* samples {(*map)["samples"]};
+         if ( samples->isArray() )
+         {
+            // make sure section is within limits of array
+            if ( section >= 0 && section < samples->toArray()->size() )
+            {
+               // return sample name
+               return samples->toArray()->at(section)->toVariant();
+            }
+         }
+      }
+
+      // if no sample name found return nothing
+      return QVariant();
    default:
+      // unknown orientation so return nothing
       return QVariant();
    }
 }
@@ -158,6 +308,7 @@ QVariant ExpressionMatrix::headerData(int section, Qt::Orientation orientation, 
 
 int ExpressionMatrix::rowCount(const QModelIndex& parent) const
 {
+   // return gene size for row count
    Q_UNUSED(parent);
    return _geneSize;
 }
@@ -169,6 +320,7 @@ int ExpressionMatrix::rowCount(const QModelIndex& parent) const
 
 int ExpressionMatrix::columnCount(const QModelIndex& parent) const
 {
+   // return sample size for column count
    Q_UNUSED(parent);
    return _sampleSize;
 }
@@ -180,12 +332,39 @@ int ExpressionMatrix::columnCount(const QModelIndex& parent) const
 
 QVariant ExpressionMatrix::data(const QModelIndex& index, int role) const
 {
+   // if role is not display return nothing
    if ( role != Qt::DisplayRole )
    {
       return QVariant();
    }
+
+   // if index is out of range return nothing
+   if ( index.row() >= _geneSize || index.column() >= _sampleSize )
+   {
+      return QVariant();
+   }
+
+   // make input variable and seek to position of queried expression
    float value;
+   if ( !seek(DATA_OFFSET+(index.row()*_sampleSize)+index.column()) )
+   {
+      E_MAKE_EXCEPTION(e);
+      e.setTitle(tr("File IO Error"));
+      e.setDetails(tr("Failed calling seek() on data object file."));
+      throw e;
+   }
+
+   // read expression from file
    stream() >> value;
+   if ( !stream() )
+   {
+      E_MAKE_EXCEPTION(e);
+      e.setTitle(tr("File IO Error"));
+      e.setDetails(tr("Failed reading from data object file."));
+      throw e;
+   }
+
+   // return expression
    return value;
 }
 
@@ -194,31 +373,25 @@ QVariant ExpressionMatrix::data(const QModelIndex& index, int role) const
 
 
 
-void ExpressionMatrix::syncGene(int index, float* expressions, Sync which)
+void ExpressionMatrix::readGene(int index, float* expressions) const
 {
-   if ( _newData )
+   // seek to position of beginning of gene's expressions
+   if ( !seek(DATA_OFFSET+(index*_sampleSize)) )
    {
-      ;//ERROR
+      E_MAKE_EXCEPTION(e);
+      e.setTitle(tr("File IO Error"));
+      e.setDetails(tr("Failed calling seek() on data object file."));
+      throw e;
    }
-   if ( !seek(DATA_OFFSET+(_sampleSize*index)) )
-   {
-      ;//ERROR
-   }
-   for (int i = 0; i < _sampleSize ;++i)
-   {
-      switch (which)
-      {
-      case Sync::Read:
-         stream() >> expressions[i];
-         break;
-      case Sync::Write:
-         stream() << expressions[i];
-         break;
-      }
-   }
+
+   // read in all expressions for gene as block of floats
+   stream().read(expressions,_sampleSize);
    if ( !stream() )
    {
-      ;//ERROR!
+      E_MAKE_EXCEPTION(e);
+      e.setTitle(tr("File IO Error"));
+      e.setDetails(tr("Failed reading from data object file."));
+      throw e;
    }
 }
 
@@ -227,13 +400,26 @@ void ExpressionMatrix::syncGene(int index, float* expressions, Sync which)
 
 
 
-void ExpressionMatrix::Gene::readGene(int index)
+void ExpressionMatrix::writeGene(int index, const float* expressions)
 {
-   if ( index < 0 || index >= _matrix->_geneSize)
+   // seek to position of beginning of gene's expressions
+   if ( !seek(DATA_OFFSET+(index*_sampleSize)) )
    {
-      ;//ERROR
+      E_MAKE_EXCEPTION(e);
+      e.setTitle(tr("File IO Error"));
+      e.setDetails(tr("Failed calling seek() on data object file."));
+      throw e;
    }
-   _matrix->syncGene(index,_expressions,Sync::Read);
+
+   // overwrite all expressions for gene as block of floats
+   stream().write(expressions,_sampleSize);
+   if ( !stream() )
+   {
+      E_MAKE_EXCEPTION(e);
+      e.setTitle(tr("File IO Error"));
+      e.setDetails(tr("Failed writing to data object file."));
+      throw e;
+   }
 }
 
 
@@ -241,13 +427,41 @@ void ExpressionMatrix::Gene::readGene(int index)
 
 
 
-void ExpressionMatrix::Gene::writeGene(int index) const
+void ExpressionMatrix::Gene::readGene(int index) const
 {
-   if ( index < 0 || index >= _matrix->_geneSize)
+   // make sure given gene index is within range
+   if ( index < 0 || index >= _matrix->_geneSize )
    {
-      ;//ERROR
+      E_MAKE_EXCEPTION(e);
+      e.setTitle(tr("Domain Error"));
+      e.setDetails(tr("Attempting to read gene %1 when maximum is %2.").arg(index)
+                   .arg(_matrix->_geneSize-1));
+      throw e;
    }
-   _matrix->syncGene(index,_expressions,Sync::Write);
+
+   // read gene expressions from data object
+   _matrix->readGene(index,_expressions);
+}
+
+
+
+
+
+
+void ExpressionMatrix::Gene::writeGene(int index)
+{
+   // make sure given gene index is within range
+   if ( index < 0 || index >= _matrix->_geneSize )
+   {
+      E_MAKE_EXCEPTION(e);
+      e.setTitle(tr("Domain Error"));
+      e.setDetails(tr("Attempting to write gene %1 when maximum is %2.").arg(index)
+                   .arg(_matrix->_geneSize-1));
+      throw e;
+   }
+
+   // write gene expressions to data object
+   _matrix->writeGene(index,_expressions);
 }
 
 
@@ -257,9 +471,16 @@ void ExpressionMatrix::Gene::writeGene(int index) const
 
 float& ExpressionMatrix::Gene::at(int index)
 {
+   // make sure given sample index is within range
    if ( index < 0 || index >= _matrix->_sampleSize )
    {
-      ;//ERROR
+      E_MAKE_EXCEPTION(e);
+      e.setTitle(tr("Domain Error"));
+      e.setDetails(tr("Attempting to access gene expression %1 when maximum is %2.").arg(index)
+                   .arg(_matrix->_sampleSize-1));
+      throw e;
    }
+
+   // return gene expression
    return _expressions[index];
 }
--- qt/src/expressionmatrix.h
+++ qt/src/expressionmatrix.h
@@ -8,6 +8,13 @@ class ExpressionMatrix : public QAbstractTableModel, public EAbstractData
 {
    Q_OBJECT
 public:
+   enum class Transform
+   {
+      None
+      ,NLog
+      ,Log2
+      ,Log10
+   };
    class Gene
    {
    public:
@@ -15,8 +22,8 @@ public:
          {}
       Gene(const Gene&) = delete;
       ~Gene() { delete _expressions; }
-      void readGene(int index);
-      void writeGene(int index) const;
+      void readGene(int index) const;
+      void writeGene(int index);
       float& at(int index);
       const float& at(int index) const { return at(index); }
       float& operator[](int index) { return _expressions[index]; }
@@ -31,16 +38,18 @@ public:
    virtual void finish() override final;
    virtual QAbstractTableModel* getModel() override final { return this; }
    void initialize(QStringList geneNames, QStringList sampleNames);
-   qint32 getGeneSize() { return _geneSize; }
-   qint32 getSampleSize() { return _sampleSize; }
+   void setTransform(Transform scale);
+   qint32 getGeneSize() const { return _geneSize; }
+   qint32 getSampleSize() const { return _sampleSize; }
+   qint64 getRawSize() const;
+   float* dumpRawData() const;
    QVariant headerData(int section, Qt::Orientation orientation, int role) const;
    int rowCount(const QModelIndex& parent) const override final;
    int columnCount(const QModelIndex& parent) const override final;
    QVariant data(const QModelIndex& index, int role) const override final;
 private:
-   enum class Sync { Read,Write };
-   void syncGene(int index, float* _expressions, Sync which);
-   bool _newData {true};
+   void readGene(int index, float* _expressions) const;
+   void writeGene(int index, const float* _expressions);
    qint32 _geneSize {0};
    qint32 _sampleSize {0};
 };
