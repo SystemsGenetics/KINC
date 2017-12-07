2a8794f71ebd96390d49c46ace8ec21c789716d4
Josh B <4ctrl.alt.del@gmail.com>
2017-12-06 16:15:39 -0800

    Added to gene pair vector class.
    Created additional functionality needed while interfacing with
    correlation matrix.

--- src/genepair_vector.cpp
+++ src/genepair_vector.cpp
@@ -30,6 +30,28 @@ Vector::Vector(qint64 geneX, qint64 geneY, qint64 cluster):
 
 
 
+void Vector::indent(qint64 indent, qint64 geneX)
+{
+   qint64 cluster = indent%_maxClusterSize;
+   qint64 geneY = indent/_maxClusterSize - geneX*(geneX - 1);
+   if ( geneX < 1 || geneY < 0 || cluster < 0 || geneY >= geneX || cluster >= _maxClusterSize )
+   {
+      E_MAKE_EXCEPTION(e);
+      e.setTitle(QObject::tr("Gene Pair Vector Error"));
+      e.setDetails(QObject::tr("Cannot initialize gene vector (%1,%2,%3).").arg(geneX).arg(geneY)
+                   .arg(cluster));
+      throw e;
+   }
+   _geneX = geneX;
+   _geneY = geneY;
+   _cluster = cluster;
+}
+
+
+
+
+
+
 void Vector::operator++()
 {
    if ( ++_cluster >= _maxClusterSize )
@@ -41,4 +63,16 @@ void Vector::operator++()
          ++_geneX;
       }
    }
+}
+
+
+
+
+
+
+Vector Vector::operator++(int)
+{
+   Vector ret {*this};
+   ++(*this);
+   return ret;
 }
--- src/genepair_vector.h
+++ src/genepair_vector.h
@@ -12,13 +12,17 @@ namespace GenePair
       Vector(qint64 geneX = 0, qint64 geneY = 0, qint64 cluster = 0);
       Vector(const Vector&) = default;
       Vector(Vector&&) = default;
+      void indent(qint64 indent, qint64 geneX);
       qint64 indent() const  { return (_geneX*(_geneX - 1)/2 + _geneY)*_maxClusterSize + _cluster; }
+      qint64 maxIndent() const
+         { return (_geneX*(_geneX - 1)/2 + _geneY)*_maxClusterSize + _maxClusterSize - 1; }
       qint64 geneX() const { return _geneX; }
       qint64 geneY() const { return _geneY; }
       qint64 cluster() const { return _cluster; }
       Vector& operator=(const Vector&) = default;
       Vector& operator=(Vector&&) = default;
       void operator++();
+      Vector operator++(int);
       bool operator==(const Vector& object)
          { return _geneX == object._geneX && _geneY == object._geneY
                   && _cluster == object._cluster; }
