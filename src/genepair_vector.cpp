54f71bf7dd7a9027cd68d7f0420aca7220de579f
Josh B <4ctrl.alt.del@gmail.com>
2017-12-11 22:07:47 -0800

    Added comments to gene pair vector.

--- src/genepair_vector.cpp
+++ src/genepair_vector.cpp
@@ -14,6 +14,7 @@ Vector::Vector(qint32 geneX, qint32 geneY):
    _geneX(geneX),
    _geneY(geneY)
 {
+   // make sure vector given is valid
    if ( geneX < 1 || geneY < 0 || geneY >= geneX )
    {
       E_MAKE_EXCEPTION(e);
@@ -30,6 +31,7 @@ Vector::Vector(qint32 geneX, qint32 geneY):
 
 qint64 Vector::indent(qint8 cluster) const
 {
+   // make sure cluster given is valid
    if ( cluster < 0 || cluster >= _maxClusterSize )
    {
       E_MAKE_EXCEPTION(e);
@@ -37,6 +39,8 @@ qint64 Vector::indent(qint8 cluster) const
       e.setDetails(QObject::tr("Cluster %1 is outside limits.").arg(cluster));
       throw e;
    }
+
+   // compute indent with given cluster and return it
    qint64 ret {_geneX};
    ret = ret*(ret - 1)/2;
    ret += _geneY;
@@ -51,8 +55,10 @@ qint64 Vector::indent(qint8 cluster) const
 
 void Vector::operator++()
 {
+   // increment gene y and check it is reaches gene x
    if ( ++_geneY >= _geneX )
    {
+      // reset gene y to 0 and increment gene x
       _geneY = 0;
       ++_geneX;
    }
@@ -65,6 +71,7 @@ void Vector::operator++()
 
 Vector Vector::operator++(int)
 {
+   // save vector value, increment it, and return previous value
    Vector ret {*this};
    ++(*this);
    return ret;
