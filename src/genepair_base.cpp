9a2ebc09bc1b500b4f4a3326b959d435dffa1f42
Josh B <4ctrl.alt.del@gmail.com>
2017-12-11 22:09:04 -0800

    Reworked gene pair base class.
    Finished making changes to base class moving most functionality and code
    seen in correlation class to this one. Now the correlation/cluster data
    types should mostly just have to implement the virtual functions
    declared in this base class along with its special pair iterator.

--- src/genepair_base.cpp
+++ src/genepair_base.cpp
@@ -21,7 +21,8 @@ void Base::readData()
    }
 
    // read in all data
-   stream() >> _geneSize >> _dataSize >> _pairSize >> _offset;
+   stream() >> _geneSize >> _dataSize >> _pairSize >> _rawPairSize >> _offset;
+   readHeader();
 
    // make sure reading worked
    if ( !stream() )
@@ -50,7 +51,8 @@ void Base::newData()
    }
 
    // write out all header information
-   stream() << _geneSize << _dataSize << _pairSize << _offset;
+   stream() << _geneSize << _dataSize << _pairSize << _rawPairSize << _offset;
+   writeHeader();
 
    // make sure writing worked
    if ( !stream() )
@@ -67,10 +69,40 @@ void Base::newData()
 
 
 
-void Base::initialize(int geneSize, int dataSize, int offset)
+const EMetadata& Base::getGeneNames() const
 {
+   // get metadata root and make sure genes key exist
+   const EMetadata::Map* map {meta().toObject()};
+   if ( !map->contains("genes") )
+   {
+      E_MAKE_EXCEPTION(e);
+      e.setTitle(QObject::tr("Null Return Reference"));
+      e.setDetails(QObject::tr("Requesting reference to gene names when none exists."));
+      throw e;
+   }
+
+   // return gene names list
+   return *(*map)["genes"];
+}
+
+
+
+
+
+
+void Base::initialize(const EMetadata& geneNames, int dataSize, int offset)
+{
+   // make sure gene names metadata is an array and is not empty
+   if ( !geneNames.isArray() || geneNames.toArray()->isEmpty() )
+   {
+      E_MAKE_EXCEPTION(e);
+      e.setTitle(QObject::tr("Domain Error"));
+      e.setDetails(QObject::tr("Gene names metadata is not an array or empty."));
+      throw e;
+   }
+
    // make sure arguments are valid
-   if ( geneSize < 2 || dataSize < 1 || offset < 0 )
+   if ( dataSize < 1 || offset < 0 )
    {
       E_MAKE_EXCEPTION(e);
       e.setTitle(QObject::tr("Gene Pair Base Initialization Error"));
@@ -78,13 +110,17 @@ void Base::initialize(int geneSize, int dataSize, int offset)
       throw e;
    }
 
+   // get map of metadata root and make copy of gene names
+   EMetadata::Map* map {meta().toObject()};
+   map->insert("genes",new EMetadata(geneNames));
+
    // initiailze new data within object
-   _geneSize = geneSize;
+   _geneSize = geneNames.toArray()->size();
    _dataSize = dataSize;
+   _offset = offset;
    _pairSize = 0;
    _rawPairSize = 0;
-   _offset = offset;
-   _lastWrite = Vector();
+   _lastWrite = 0;
 }
 
 
@@ -94,6 +130,15 @@ void Base::initialize(int geneSize, int dataSize, int offset)
 
 void Base::write(Vector index, qint8 cluster)
 {
+   // make sure this is new data object that can be written to
+   if ( _lastWrite == -1 )
+   {
+      E_MAKE_EXCEPTION(e);
+      e.setTitle(QObject::tr("Gene Pair Base Logical Error"));
+      e.setDetails(QObject::tr("Attempting to write data to uninitialized object."));
+      throw e;
+   }
+
    // make sure the new gene pair has a higher indent than the previous written so the list of
    // all indents are sorted
    if ( index.indent(cluster) >= _lastWrite )
@@ -107,7 +152,7 @@ void Base::write(Vector index, qint8 cluster)
 
    // seek to position for next gene pair and write indent value
    seekPair(_rawPairSize);
-   stream() << index.geneX() << index.geneY << cluster;
+   stream() << index.geneX() << index.geneY() << cluster;
 
    // make sure writing worked
    if ( !stream() )
@@ -160,10 +205,12 @@ qint64 Base::findPair(qint64 indent, qint64 first, qint64 last) const
    qint64 pivot {first + (last - first)/2};
    seekPair(pivot);
 
-   // read in indent and geneX value of gene pair
-   qint64 readIndent;
-   qint64 geneX;
-   stream() >> readIndent >> geneX;
+   // read in gene pair item header
+   qint32 geneX;
+   qint32 geneY;
+   qint8 cluster;
+   stream() >> geneX >> geneY >> cluster;
+   Vector index(geneX,geneY);
 
    // make sure reading worked
    if ( !stream() )
@@ -175,16 +222,16 @@ qint64 Base::findPair(qint64 indent, qint64 first, qint64 last) const
    }
 
    // if indent values match return index
-   if ( readIndent == indent )
+   if ( index.indent(cluster) == indent )
    {
       return pivot;
    }
 
-   // else if first and last are not the same divide and conquer
+   // else if the list of indexes is greater than 1 divide it in half and figure out which side
+   // to divide and conquer depending on the value of the pivot
    else if ( first != last )
    {
-      // if indent is less than pivot divide lower half
-      if ( readIndent > indent )
+      if ( index.indent(cluster) > indent )
       {
          // if pivot is first add one so pivot is not less than first when passed
          if ( pivot == first )
@@ -193,8 +240,6 @@ qint64 Base::findPair(qint64 indent, qint64 first, qint64 last) const
          }
          return findPair(indent,first,pivot - 1);
       }
-
-      // else it is greater so divide upper half
       else
       {
          // if pivot is last decrement one so pivot is not greater than last when passed
@@ -237,5 +282,127 @@ void Base::seekPair(qint64 index) const
       e.setTitle(QObject::tr("File IO Error"));
       e.setDetails(QObject::tr("Failed calling seek() on data object file."));
       throw e;
+   }
+}
+
+
+
+
+
+
+void Base::Pair::write(Vector index)
+{
+   // make sure cluster size of gene pair does not exceed max
+   if ( clusterSize() > Vector::_maxClusterSize )
+   {
+      E_MAKE_EXCEPTION(e);
+      e.setTitle(QObject::tr("Gene Pair Logical Error"));
+      e.setDetails(QObject::tr("Cannot write gene pair with cluster size of %1 exceeding the max of"
+                               " %2.").arg(clusterSize()).arg(Vector::_maxClusterSize));
+      throw e;
+   }
+
+   // go through each cluster and write it to data object
+   for (int i = 0; i < clusterSize() ;++i)
+   {
+      _matrix->write(index,i);
+      writeCluster(_matrix->stream(),i);
+   }
+
+   // increment pair size of data object
+   ++(_matrix->_pairSize);
+}
+
+
+
+
+
+
+void Base::Pair::read(Vector index) const
+{
+   // clear any existing clusters
+   clearClusters();
+
+   // attempt to find vector within data object
+   qint64 index_;
+   if ( (index_ = _cMatrix->findPair(index.indent(0),0,_cMatrix->_rawPairSize - 1)) != -1 )
+   {
+      // gene pair with vector found, read in all clusters
+      _nextIndex = index_;
+      readNext();
+   }
+}
+
+
+
+
+
+
+void Base::Pair::readFirst() const
+{
+   // reset next index to first gene pair and read it
+   _nextIndex = 0;
+   readNext();
+}
+
+
+
+
+
+
+void Base::Pair::readNext() const
+{
+   // make sure read next index is not already at end of data object
+   if ( _nextIndex < _cMatrix->_rawPairSize )
+   {
+      // clear any existing clusters
+      clearClusters();
+
+      // get to first cluster
+      qint8 cluster;
+      Vector vector {_cMatrix->getPair(_nextIndex++,&cluster)};
+
+      // make sure this is cluster 0
+      if ( cluster != 0 )
+      {
+         E_MAKE_EXCEPTION(e);
+         e.setTitle(QObject::tr("File IO Error"));
+         e.setDetails(QObject::tr("Reading gene pair failed because first cluster is not 0."));
+         throw e;
+      }
+
+      // add first cluster, read it in, and save what vector this gene pair is
+      addCluster();
+      readCluster(_cMatrix->stream(),0);
+      _vector = vector;
+
+      // read in remaining clusters for gene pair
+      qint8 count {1};
+      while ( _nextIndex < _cMatrix->_rawPairSize )
+      {
+         // get next gene pair cluster
+         _cMatrix->getPair(_nextIndex++,&cluster);
+
+         // if cluster is zero this is the next gene pair so break from loop
+         if ( cluster == 0 )
+         {
+            --_nextIndex;
+            break;
+         }
+
+         // make sure max cluster size has not been exceeded
+         if ( ++count > Vector::_maxClusterSize )
+         {
+            E_MAKE_EXCEPTION(e);
+            e.setTitle(QObject::tr("File IO Error"));
+            e.setDetails(QObject::tr("Reading gene pair failed because it exceeds the max number of"
+                                     " clusters."));
+            throw e;
+         }
+
+         // add new cluster and read it in
+         addCluster();
+         readCluster(_cMatrix->stream(),cluster);
+      }
    }
 }
--- src/genepair_base.h
+++ src/genepair_base.h
@@ -1,6 +1,7 @@
 #ifndef GENEPAIR_BASE_H
 #define GENEPAIR_BASE_H
 #include <ace/core/AceCore.h>
+#include <ace/core/metadata.h>
 
 #include "genepair_vector.h"
 
@@ -14,40 +15,53 @@ namespace GenePair
       class Pair
       {
       public:
-         Pair(Base* matrix);
-         Pair(const Base* matrix);
-         virtual void clear() = 0;
-         virtual void addCluster() = 0;
+         Pair(Base* matrix):
+            _matrix(matrix),
+            _cMatrix(matrix),
+            _vector({matrix->_geneSize,0})
+            {}
+         Pair(const Base* matrix):
+            _cMatrix(matrix),
+            _vector({matrix->_geneSize,0})
+            {}
+         Pair(const Pair&) = default;
+         Pair(Pair&&) = default;
+         virtual void clearClusters() const = 0;
+         virtual void addCluster(int amount = 1) const = 0;
+         virtual int clusterSize() const;
+         virtual bool isEmpty() const;
          void write(Vector index);
          void read(Vector index) const;
          void readFirst() const;
          void readNext() const;
-         bool hasNext() const;
-         int clusterSize() const;
-         bool isEmpty() const;
-         const Vector& index() const;
+         bool hasNext() const { return _nextIndex != _cMatrix->_rawPairSize; }
+         const Vector& vector() const { return _vector; }
+         Pair& operator=(const Pair&) = default;
+         Pair& operator=(Pair&&) = default;
       protected:
          virtual void writeCluster(EDataStream& stream, int cluster) = 0;
-         virtual void readCluster(EDataStream& stream, int cluster) = 0;
+         virtual void readCluster(const EDataStream& stream, int cluster) const = 0;
       private:
          Base* _matrix {nullptr};
          const Base* _cMatrix;
-         Vector _index;
+         mutable qint64 _nextIndex {0};
+         mutable Vector _vector;
       };
       virtual void readData() override final;
       virtual quint64 getDataEnd() const override final
          { return _headerSize + _offset + _pairSize*(_dataSize + _itemHeaderSize); }
       virtual void newData() override final;
       virtual void prepare(bool) override final {}
-      virtual void finish() override final { Base::newData(); }
+      virtual void finish() override final { newData(); }
       qint64 size() const { return _pairSize; }
+      const EMetadata& getGeneNames() const;
    protected:
       virtual void writeHeader() = 0;
       virtual void readHeader() = 0;
-      void initialize(int geneSize, int dataSize, int offset);
+      void initialize(const EMetadata& geneNames, int dataSize, int offset);
    private:
-      void write(Vector index);
-      Vector getPair(qint64 index) const;
+      void write(Vector index, qint8 cluster);
+      Vector getPair(qint64 index, qint8* cluster) const;
       qint64 findPair(qint64 indent, qint64 first, qint64 last) const;
       void seekPair(qint64 index) const;
       constexpr static int _headerSize {24};
@@ -57,7 +71,7 @@ namespace GenePair
       qint64 _pairSize {0};
       qint64 _rawPairSize {0};
       qint16 _offset {0};
-      qint64 _lastWrite;
+      qint64 _lastWrite {-1};
    };
 }
 
