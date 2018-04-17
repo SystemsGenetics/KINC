e482cdbd9f9cf62905204778b24f9350214c46af
Ben Shealy <bentshermann@gmail.com>
2018-04-17 10:28:43 -0400

    Added comparison operators to GenePair::Vector

--- src/genepair_vector.h
+++ src/genepair_vector.h
@@ -24,7 +24,15 @@ namespace GenePair
       bool operator==(const Vector& object)
          { return _geneX == object._geneX && _geneY == object._geneY; }
       bool operator!=(const Vector& object)
-         { return _geneX != object._geneX || _geneY != object._geneY; }
+         { return !(*this == object); }
+      bool operator<(const Vector& object)
+         { return _geneX < object._geneX || (_geneX == object._geneX && _geneY < object._geneY); }
+      bool operator<=(const Vector& object)
+         { return *this < object || *this == object; }
+      bool operator>(const Vector& object)
+         { return !(*this <= object); }
+      bool operator>=(const Vector& object)
+         { return !(*this < object); }
       constexpr static qint8 MAX_CLUSTER_SIZE {64};
    private:
       qint32 _geneX {1};
