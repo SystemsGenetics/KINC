c5bbe15ba0f27a2cf2f5151c59365103ea374acc
Ben Shealy <bentshermann@gmail.com>
2018-05-31 14:04:58 -0400

    Added initial scripts for testing GMM and VB-GMM

---
+++ scripts/test-gmm.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import sklearn.mixture
+import sys
+
+
+
+if __name__ == "__main__":
+	if len(sys.argv) != 2:
+		print "usage: python test-gmm.py [infile]"
+		sys.exit(1)
+
+	# load data
+	df = pd.read_csv(sys.argv[1], sep="\t")
+
+	# iterate through each pair
+	for i in xrange(len(df.index)):
+		for j in xrange(i):
+			# extract pairwise data
+			X = df.iloc[[i, j]].dropna(axis=1, how="any")
+			X = X.values.T
+			N = X.shape[0]
+
+			# make sure there are enough samples
+			min_K = -1
+			min_crit = float("inf")
+
+			if N > 30:
+				# initialize clustering models
+				models = [sklearn.mixture.GaussianMixture(n_components=n+1) for n in xrange(5)]
+
+				# identify number of clusters
+				for k, model in enumerate(models):
+					# fit model
+					model.fit(X)
+
+					# save the best model
+					crit = model.aic(X)
+					if crit < min_crit:
+						min_K = len(model.weights_)
+						min_crit = crit
+
+				# plot clustering results
+				plt.subplots(1, len(models), True, True, figsize=(5 * len(models), 5))
+
+				for k, model in enumerate(models):
+					K = len(model.weights_)
+					crit = model.aic(X)
+					y = model.predict(X)
+
+					plt.subplot(1, len(models), k + 1)
+					plt.scatter(X[:, 0], X[:, 1], s=20, c=y, cmap="brg")
+					plt.title("N = %d, K = %d, crit = %g" % (N, K, crit))
+					plt.xlabel(df.index[i])
+					plt.ylabel(df.index[j])
+
+				plt.show()
+
+			print "%4d %4d: N = %4d, K = %4d" % (i, j, N, min_K)
---
+++ scripts/test-vbgmm.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import sklearn.mixture
+import sys
+
+
+
+if __name__ == "__main__":
+	if len(sys.argv) != 2:
+		print "usage: python test-vbgmm.py [infile]"
+		sys.exit(1)
+
+	# load data
+	df = pd.read_csv(sys.argv[1], sep="\t")
+
+	# iterate through each pair
+	for i in xrange(len(df.index)):
+		for j in xrange(i):
+			# extract pairwise data
+			X = df.iloc[[i, j]].dropna(axis=1, how="any")
+			X = X.values.T
+			N = X.shape[0]
+
+			# make sure there are enough samples
+			K = -1
+
+			if X.shape[0] > 30:
+				# initialize clustering model
+				model = sklearn.mixture.BayesianGaussianMixture(n_components=5, weight_concentration_prior=1e3)
+
+				# fit clustering model
+				model.fit(X)
+
+				print "".join(["%8.3f" % w for w in model.weights_])
+
+				# compute number of effective components
+				K = sum([1 for w in model.weights_ if w > 0.05])
+
+				# plot clustering results
+				y = model.predict(X)
+				plt.scatter(X[:, 0], X[:, 1], s=20, c=y, cmap="brg")
+				plt.title("N = %d, K = %d" % (N, K))
+				plt.xlabel(df.index[i])
+				plt.ylabel(df.index[j])
+				plt.show()
+
+			print "%4d %4d: N = %4d, K = %4d" % (i, j, N, K)
