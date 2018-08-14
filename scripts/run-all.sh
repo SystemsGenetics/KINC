1070e578e9b78966894f458033fb8e27a97cf2c0
Ben Shealy <bentshermann@gmail.com>
2018-08-14 16:59:49 -0400

    Added run-all.sh for basic end-to-end testing

---
+++ scripts/run-all.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -x
+
+KINC="build/cli/kinc"
+DATA="data"
+LOGS="logs"
+
+INFILE="$1"
+
+# import emx
+EMX_FILE="$DATA/$(basename $INFILE .txt).emx"
+
+$KINC run import-emx --input $INFILE --output $EMX_FILE --nan NA
+
+# similarity
+CCM_FILE="$DATA/$(basename $EMX_FILE .emx).ccm"
+CMX_FILE="$DATA/$(basename $EMX_FILE .emx).cmx"
+CLUSMETHOD="none"
+CORRMETHOD="pearson"
+
+$KINC run similarity --input $EMX_FILE --ccm $CCM_FILE --cmx $CMX_FILE --clusmethod $CLUSMETHOD --corrmethod $CORRMETHOD
+
+# threshold
+LOG_FILE="$LOGS/$(basename $CMX_FILE .cmx).txt"
+
+mkdir -p $LOGS
+
+$KINC run rmt --input $CMX_FILE --log $LOG_FILE
+
+# export cmx
+OUTFILE="$DATA/$(basename $CMX_FILE .cmx)-cmx.txt"
+
+$KINC run export-cmx --ccm $CCM_FILE --cmx $CMX_FILE --output $OUTFILE
+
+# extract
+NET_FILE="$DATA/$(basename $EMX_FILE .emx)-net.txt"
+GML_FILE="$DATA/$(basename $EMX_FILE .emx).graphml"
+THRESHOLD=0.85
+
+$KINC run extract --emx $EMX_FILE --ccm $CCM_FILE --cmx $CMX_FILE --output $NET_FILE --graphml $GML_FILE --mincorr $THRESHOLD
