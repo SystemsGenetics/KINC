72e03b8b3edde48b52ea34cccbf9915d9863621c
Ben Shealy <bentshermann@gmail.com>
2018-11-28 12:04:24 -0500

    Updated run-all-py.sh

--- scripts/run-all-py.sh
+++ scripts/run-all-py.sh
@@ -6,56 +6,68 @@ if [[ $# != 1 ]]; then
 	exit -1
 fi
 
+# define analytic flags
+DO_SIMILARITY=1
+DO_THRESHOLD=1
+DO_EXTRACT=1
+
+# define input/output files
 DATA="data"
 EMX_FILE="$1"
+CMX_FILE="$DATA/$(basename $EMX_FILE .txt)-cmx-py.txt"
+NET_FILE="$DATA/$(basename $EMX_FILE .txt)-net-py.txt"
 
 # similarity
-CMX_FILE="$DATA/$(basename $EMX_FILE .txt)-cmx-py.txt"
-CLUSMETHOD="gmm"
-CORRMETHOD="pearson"
-MINEXPR="-inf"
-MINCLUS=1
-MAXCLUS=5
-CRITERION="bic"
-PREOUT="--preout"
-POSTOUT="--postout"
-MINCORR=0.5
-MAXCORR=1
+if [[ $DO_SIMILARITY = 1 ]]; then
+	CLUSMETHOD="gmm"
+	CORRMETHOD="pearson"
+	MINEXPR="-inf"
+	MINCLUS=1
+	MAXCLUS=5
+	CRITERION="bic"
+	PREOUT="--preout"
+	POSTOUT="--postout"
+	MINCORR=0
+	MAXCORR=1
 
-python scripts/similarity.py \
-   -i $EMX_FILE \
-   -o $CMX_FILE \
-   --clusmethod $CLUSMETHOD \
-   --corrmethod $CORRMETHOD \
-   --minexpr=$MINEXPR \
-   --minclus $MINCLUS --maxclus $MAXCLUS \
-   --crit $CRITERION \
-   $PREOUT $POSTOUT \
-   --mincorr $MINCORR --maxcorr $MAXCORR
+	python scripts/similarity.py \
+	   -i $EMX_FILE \
+	   -o $CMX_FILE \
+	   --clusmethod $CLUSMETHOD \
+	   --corrmethod $CORRMETHOD \
+	   --minexpr=$MINEXPR \
+	   --minclus $MINCLUS --maxclus $MAXCLUS \
+	   --crit $CRITERION \
+	   $PREOUT $POSTOUT \
+	   --mincorr $MINCORR --maxcorr $MAXCORR
+fi
 
 # threshold
-NUM_GENES=$(expr $(cat $EMX_FILE | wc -l) - 1)
-METHOD="rmt"
-TSTART=0.99
-TSTEP=0.001
-TSTOP=0.50
+if [[ $DO_THRESHOLD = 1 ]]; then
+	NUM_GENES=$(expr $(cat $EMX_FILE | wc -l) - 1)
+	METHOD="rmt"
+	TSTART=0.99
+	TSTEP=0.001
+	TSTOP=0.50
 
-python scripts/threshold.py \
-   -i $CMX_FILE \
-   --genes $NUM_GENES \
-   --method $METHOD \
-   --tstart $TSTART \
-   --tstep $TSTEP \
-   --tstop $TSTOP
+	python scripts/threshold.py \
+	   -i $CMX_FILE \
+	   --genes $NUM_GENES \
+	   --method $METHOD \
+	   --tstart $TSTART \
+	   --tstep $TSTEP \
+	   --tstop $TSTOP
+fi
 
 # extract
-NET_FILE="$DATA/$(basename $EMX_FILE .txt)-net-py.txt"
-MINCORR=0
-MAXCORR=1
+if [[ $DO_EXTRACT = 1 ]]; then
+	MINCORR=0
+	MAXCORR=1
 
-python scripts/extract.py \
-   --emx $EMX_FILE \
-   --cmx $CMX_FILE \
-   --output $NET_FILE \
-   --mincorr $MINCORR \
-   --maxcorr $MAXCORR
+	python scripts/extract.py \
+	   --emx $EMX_FILE \
+	   --cmx $CMX_FILE \
+	   --output $NET_FILE \
+	   --mincorr $MINCORR \
+	   --maxcorr $MAXCORR
+fi
