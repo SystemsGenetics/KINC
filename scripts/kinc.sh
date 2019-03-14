c89af33097f3216e110a16b9a3b119073f446f01
Ben Shealy <bentshermann@gmail.com>
2019-03-14 15:04:50 -0400

    Worked on kinc.sh

--- scripts/kinc.sh
+++ scripts/kinc.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # parse command-line arguments
-if [[ $# != 2 ]]; then
-	echo "usage: $0 <mode> <infile>"
+if [[ $# != 3 ]]; then
+	echo "usage: $0 <mode> <np> <infile>"
 	exit -1
 fi
 
 # define execution mode
 MODE="$1"
+NP="$2"
 
 # define analytic flags
 DO_IMPORT_EMX=1
@@ -17,7 +18,7 @@ DO_THRESHOLD=0
 DO_EXTRACT=0
 
 # define input/output files
-INFILE="$2"
+INFILE="$3"
 DATA="data"
 EMX_FILE="$DATA/$(basename $INFILE .txt).emx"
 CCM_FILE="$DATA/$(basename $EMX_FILE .emx).ccm"
@@ -26,26 +27,18 @@ LOGS="logs"
 RMT_FILE="$LOGS/$(basename $CMX_FILE .cmx).txt"
 
 # apply settings
+kinc settings set threads 2
+kinc settings set logging off
+
 if [[ $MODE = "cuda" ]]; then
 	kinc settings set cuda 0
 	kinc settings set opencl none
-	kinc settings set threads 2
-	kinc settings set logging off
-
-	NP=1
 elif [[ $MODE = "opencl" ]]; then
 	kinc settings set cuda none
 	kinc settings set opencl 0:0
-	kinc settings set threads 2
-	kinc settings set logging off
-
-	NP=1
 elif [[ $MODE == "serial" ]]; then
 	kinc settings set cuda none
 	kinc settings set opencl none
-	kinc settings set logging off
-
-	NP=$(nproc)
 else
 	echo "error: invalid execution mode \'$MODE\'"
 	exit -1
@@ -67,8 +60,8 @@ if [[ $DO_SIMILARITY = 1 ]]; then
 	MINCLUS=1
 	MAXCLUS=5
 	CRITERION="BIC"
-	PREOUT="--preout"
-	POSTOUT="--postout"
+	PREOUT="true"
+	POSTOUT="true"
 	MINCORR=0.5
 	MAXCORR=1
 	LSIZE=32
@@ -83,7 +76,8 @@ if [[ $DO_SIMILARITY = 1 ]]; then
 		--minclus $MINCLUS \
 		--maxclus $MAXCLUS \
 		--crit $CRITERION \
-		$PREOUT $POSTOUT \
+		--preout $PREOUT \
+		--postout $POSTOUT \
 		--mincorr $MINCORR \
 		--maxcorr $MAXCORR \
 		--lsize $LSIZE
