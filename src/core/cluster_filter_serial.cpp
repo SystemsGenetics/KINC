963bdbd3ae88d42fd9f48e42adb42aef438635b1
Stephen Ficklin <spficklin@gmail.com>
2019-06-09 22:15:26 -0700

    periodic checkin

--- src/core/cluster_filter_serial.cpp
+++ src/core/cluster_filter_serial.cpp
@@ -42,45 +42,44 @@ std::unique_ptr<EAbstractAnalyticBlock> ClusterFilter::Serial::execute(const EAb
       ELog() << tr("Executing(serial) work index %1.\n").arg(block->index());
    }
 
-   // cast block to work block
+   // Cast block to work block.
    const WorkBlock* workBlock {block->cast<WorkBlock>()};
 
-   // initialize result block
+   // Initialize result block.
    ResultBlock* resultBlock {new ResultBlock(workBlock->index(), workBlock->start())};
 
-   // iterate through all pairs
-   Pairwise::Index index {workBlock->start()};
-
-   // create iterators for the CCM and CMX data objects.
+   // Create iterators for the CCM and CMX data objects.
    CCMatrix::Pair ccmPair = CCMatrix::Pair(_base->_ccm);
    CorrelationMatrix::Pair cmxPair = CorrelationMatrix::Pair(_base->_cmx);
 
-   // move to the location in the CCM/CMX matrix where the work block starts.
-   ccmPair.read(index);
+   // Move to the location in the CCM/CMX matrix where the work block starts.
+   Pairwise::Index index {workBlock->start()};
    cmxPair.read(index);
+   ccmPair.read(cmxPair.index());
 
-   // iterate through the elements in the workblock.
-   for ( int i = 0; i < workBlock->size(); ++i )
+   // Iterate through the elements in the workblock.
+   qint64 block_size = workBlock->size();
+   for ( qint64 i = 0; i < block_size; ++i )
    {
        // Get the number of samples and clusters.
-       int num_clusters = cmxPair.clusterSize();
+       int num_clusters = ccmPair.clusterSize();
        int num_samples = _base->_emx->sampleSize();
 
-       // How many clusters will remain?
+       // This will store how many clusters will remain.
        qint8 num_final_K = 0;
-
-       // Get the list of correlations and labels for each cluster.
-       QVector<float> correlations = cmxPair.correlations();
-       QVector<qint8> labels(num_samples, 0);
 
        // Initialize new correlation and labels lists
        QVector<float> new_correlations;
        QVector<qint8> new_labels;
        QVector<int> k_num_samples(num_clusters, 0);
 
-       // Rebuild the pair labels.
+       // Get the list of correlations
+       QVector<float> correlations = cmxPair.correlations();
+
+       // Rebuild the pair labels from the pair sample mask.
+       QVector<qint8> labels(num_samples, 0);
        for ( qint8 k = 0; k < num_clusters; k++ ) {
-           for ( int j = 0; j < num_samples; j++ ) {
+          for ( int j = 0; j < num_samples; j++ ) {
              qint8 val = ccmPair.at(k, j);
              if (val == 1) {
                labels[j] = k;
@@ -89,10 +88,10 @@ std::unique_ptr<EAbstractAnalyticBlock> ClusterFilter::Serial::execute(const EAb
              else {
                labels[j] = -val;
              }
-           }
+          }
        }
 
-       // iterate through the clusters.
+       // Iterate through the clusters and perform a correlation power analysis.
        for ( qint8 k = 0; k < num_clusters; k++ ) {
 
            // Do the correlation power test. The following code is modeled
@@ -102,7 +101,7 @@ std::unique_ptr<EAbstractAnalyticBlock> ClusterFilter::Serial::execute(const EAb
            // compare the calculated power to that expected by the user.
            // This code uses functions from the Keith OHare StatsLib at
            // https://www.kthohr.com/statslib.html
-           int n = k_num_samples[k];;
+           int n = k_num_samples[k];
            double r = correlations[k];
            double sig_level = _base->_powerThresholdAlpha;
            double ttt = stats::qt(sig_level / 2, n - 2);
@@ -114,7 +113,9 @@ std::unique_ptr<EAbstractAnalyticBlock> ClusterFilter::Serial::execute(const EAb
                           stats::pnorm((-zr - zrc) * sqrt(n - 3), 0.0, 1);
 
            // If the calculated power is >= the expected power then we
-           // can keep this cluster.
+           // can keep this cluster.  We keep it by adding the correlation
+           // and the labels from the original cluster into new variables
+           // for the correlation and labels.
            if (power >= _base->_powerThresholdPower) {
              new_correlations.append(correlations[k]);
              new_labels = labels;
@@ -122,7 +123,8 @@ std::unique_ptr<EAbstractAnalyticBlock> ClusterFilter::Serial::execute(const EAb
            }
        }
 
-       // save pairwise output data
+       // Prepare the workblock results by adding a new pair. A pair
+       // gets added regardless if there are any clusters in it.
        Pair pair;
        pair.K = num_final_K;
        if (num_final_K > 0) {
@@ -131,11 +133,12 @@ std::unique_ptr<EAbstractAnalyticBlock> ClusterFilter::Serial::execute(const EAb
        }
        resultBlock->append(pair);
 
-       // move to the location in the CCM/CMX matrix where the work block starts.
-       ccmPair.readNext();
+       // Move to the location in the CCM/CMX matrix where the work block starts.
        cmxPair.readNext();
+       ccmPair.read(cmxPair.index());
    }
-   // return result block
+
+   // We're done! Return the result block.
    return unique_ptr<EAbstractAnalyticBlock>(resultBlock);
 }
 
