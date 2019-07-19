fac1a3006ab112a710d663e36173917ae5f57fc3
Ben Shealy <bentshermann@gmail.com>
2019-07-19 08:46:08 -0400

    Changed bash scripts to use ${} syntax for variables

--- scripts/kinc-py.sh
+++ scripts/kinc-py.sh
@@ -13,13 +13,13 @@ DO_EXTRACT=1
 
 # define input/output files
 INFILE="$1"
-DIRNAME="$(dirname $INFILE)"
-BASENAME="$(basename $INFILE .txt)"
-EMX_FILE="$INFILE"
-CMX_FILE="$DIRNAME/$BASENAME.cmx-py.txt"
+DIRNAME="$(dirname ${INFILE})"
+BASENAME="$(basename ${INFILE} .txt)"
+EMX_FILE="${INFILE}"
+CMX_FILE="${DIRNAME}/${BASENAME}.cmx-py.txt"
 
 # similarity
-if [[ $DO_SIMILARITY = 1 ]]; then
+if [[ ${DO_SIMILARITY} = 1 ]]; then
 	CLUSMETHOD="gmm"
 	CORRMETHOD="spearman"
 	MINEXPR="-inf"
@@ -32,47 +32,47 @@ if [[ $DO_SIMILARITY = 1 ]]; then
 	MAXCORR=1
 
 	env time -f "%e" python scripts/kinc-similarity.py \
-		--input $EMX_FILE \
-		--output $CMX_FILE \
-		--clusmethod $CLUSMETHOD \
-		--corrmethod $CORRMETHOD \
-		--minexpr=$MINEXPR \
-		--minclus $MINCLUS \
-		--maxclus $MAXCLUS \
-		--criterion $CRITERION \
-		$PREOUT \
-		$POSTOUT \
-		--mincorr $MINCORR \
-		--maxcorr $MAXCORR
+		--input ${EMX_FILE} \
+		--output ${CMX_FILE} \
+		--clusmethod ${CLUSMETHOD} \
+		--corrmethod ${CORRMETHOD} \
+		--minexpr=${MINEXPR} \
+		--minclus ${MINCLUS} \
+		--maxclus ${MAXCLUS} \
+		--criterion ${CRITERION} \
+		${PREOUT} \
+		${POSTOUT} \
+		--mincorr ${MINCORR} \
+		--maxcorr ${MAXCORR}
 fi
 
 # threshold
-if [[ $DO_THRESHOLD = 1 ]]; then
-	NUM_GENES=$(expr $(cat $EMX_FILE | wc -l) - 1)
+if [[ ${DO_THRESHOLD} = 1 ]]; then
+	NUM_GENES=$(expr $(cat ${EMX_FILE} | wc -l) - 1)
 	METHOD="rmt"
 	TSTART=0.99
 	TSTEP=0.001
 	TSTOP=0.50
 
 	env time -f "%e" python scripts/kinc-threshold.py \
-		--input $CMX_FILE \
-		--n-genes $NUM_GENES \
-		--method $METHOD \
-		--tstart $TSTART \
-		--tstep $TSTEP \
-		--tstop $TSTOP
+		--input ${CMX_FILE} \
+		--n-genes ${NUM_GENES} \
+		--method ${METHOD} \
+		--tstart ${TSTART} \
+		--tstep ${TSTEP} \
+		--tstop ${TSTOP}
 fi
 
 # extract
-if [[ $DO_EXTRACT = 1 ]]; then
+if [[ ${DO_EXTRACT} = 1 ]]; then
 	MINCORR=0
 	MAXCORR=1
-	NET_FILE="$DIRNAME/$BASENAME.th000.coexpnet-py.txt"
+	NET_FILE="${DIRNAME}/${BASENAME}.th000.coexpnet-py.txt"
 
 	env time -f "%e" python scripts/kinc-extract.py \
-		--emx $EMX_FILE \
-		--cmx $CMX_FILE \
-		--output $NET_FILE \
-		--mincorr $MINCORR \
-		--maxcorr $MAXCORR
+		--emx ${EMX_FILE} \
+		--cmx ${CMX_FILE} \
+		--output ${NET_FILE} \
+		--mincorr ${MINCORR} \
+		--maxcorr ${MAXCORR}
 fi
--- scripts/kinc.sh
+++ scripts/kinc.sh
@@ -20,42 +20,42 @@ DO_EXTRACT=1
 
 # define input/output files
 INFILE="$3"
-DIRNAME="$(dirname $INFILE)"
-BASENAME="$(basename $INFILE .txt)"
-EMX_FILE="$DIRNAME/$BASENAME.emx"
-CCM_FILE="$DIRNAME/$BASENAME.ccm"
-CMX_FILE="$DIRNAME/$BASENAME.cmx"
-CMX_TXT_FILE="$DIRNAME/$BASENAME.cmx.txt"
-RMT_FILE="$DIRNAME/$BASENAME.rmt.txt"
+DIRNAME="$(dirname ${INFILE})"
+BASENAME="$(basename ${INFILE} .txt)"
+EMX_FILE="${DIRNAME}/${BASENAME}.emx"
+CCM_FILE="${DIRNAME}/${BASENAME}.ccm"
+CMX_FILE="${DIRNAME}/${BASENAME}.cmx"
+CMX_TXT_FILE="${DIRNAME}/${BASENAME}.cmx.txt"
+RMT_FILE="${DIRNAME}/${BASENAME}.rmt.txt"
 
 # apply settings
 kinc settings set threads 2
 kinc settings set logging off
 
-if [[ $MODE = "cuda" ]]; then
+if [[ ${MODE} = "cuda" ]]; then
 	kinc settings set cuda 0
 	kinc settings set opencl none
-elif [[ $MODE = "opencl" ]]; then
+elif [[ ${MODE} = "opencl" ]]; then
 	kinc settings set cuda none
 	kinc settings set opencl 0:0
-elif [[ $MODE == "serial" ]]; then
+elif [[ ${MODE} == "serial" ]]; then
 	kinc settings set cuda none
 	kinc settings set opencl none
 else
-	echo "error: invalid execution mode \'$MODE\'"
+	echo "error: invalid execution mode \'${MODE}\'"
 	exit -1
 fi
 
 # import emx
-if [[ $DO_IMPORT_EMX = 1 ]]; then
+if [[ ${DO_IMPORT_EMX} = 1 ]]; then
 	env time -f "%e" kinc run import-emx \
-		--input $INFILE \
-		--output $EMX_FILE \
+		--input ${INFILE} \
+		--output ${EMX_FILE} \
 		--nan NA
 fi
 
 # similarity
-if [[ $DO_SIMILARITY = 1 ]]; then
+if [[ ${DO_SIMILARITY} = 1 ]]; then
 	CLUSMETHOD="gmm"
 	CORRMETHOD="spearman"
 	MINEXPR="-inf"
@@ -68,67 +68,67 @@ if [[ $DO_SIMILARITY = 1 ]]; then
 	MAXCORR=1
 	LSIZE=32
 
-	env time -f "%e" mpirun -np $NP kinc run similarity \
-		--input $EMX_FILE \
-		--ccm $CCM_FILE \
-		--cmx $CMX_FILE \
-		--clusmethod $CLUSMETHOD \
-		--corrmethod $CORRMETHOD \
-		--minexpr $MINEXPR \
-		--minclus $MINCLUS \
-		--maxclus $MAXCLUS \
-		--crit $CRITERION \
-		--preout $PREOUT \
-		--postout $POSTOUT \
-		--mincorr $MINCORR \
-		--maxcorr $MAXCORR \
-		--lsize $LSIZE
+	env time -f "%e" mpirun -np ${NP} kinc run similarity \
+		--input ${EMX_FILE} \
+		--ccm ${CCM_FILE} \
+		--cmx ${CMX_FILE} \
+		--clusmethod ${CLUSMETHOD} \
+		--corrmethod ${CORRMETHOD} \
+		--minexpr ${MINEXPR} \
+		--minclus ${MINCLUS} \
+		--maxclus ${MAXCLUS} \
+		--crit ${CRITERION} \
+		--preout ${PREOUT} \
+		--postout ${POSTOUT} \
+		--mincorr ${MINCORR} \
+		--maxcorr ${MAXCORR} \
+		--lsize ${LSIZE}
 fi
 
 # correlation power analysis
-if [[ $DO_CORRPOWER = 1 ]]; then
-	CCM_OUT_FILE="$DIRNAME/$BASENAME.corrpower.ccm"
-	CMX_OUT_FILE="$DIRNAME/$BASENAME.corrpower.cmx"
+if [[ ${DO_CORRPOWER} = 1 ]]; then
+	CCM_OUT_FILE="${DIRNAME}/${BASENAME}.corrpower.ccm"
+	CMX_OUT_FILE="${DIRNAME}/${BASENAME}.corrpower.cmx"
 	ALPHA=0.001
 	POWER=0.8
 
-	env time -f "%e" mpirun -np $NP kinc run corrpower \
-		--ccm-in $CCM_FILE \
-		--cmx-in $CMX_FILE \
-		--ccm-out $CCM_OUT_FILE \
-		--cmx-out $CMX_OUT_FILE \
-		--alpha $ALPHA \
-		--power $POWER
+	env time -f "%e" mpirun -np ${NP} kinc run corrpower \
+		--ccm-in ${CCM_FILE} \
+		--cmx-in ${CMX_FILE} \
+		--ccm-out ${CCM_OUT_FILE} \
+		--cmx-out ${CMX_OUT_FILE} \
+		--alpha ${ALPHA} \
+		--power ${POWER}
 fi
 
 # export cmx
-if [[ $DO_EXPORT_CMX = 1 ]]; then
+if [[ ${DO_EXPORT_CMX} = 1 ]]; then
 	env time -f "%e" kinc run export-cmx \
-		--emx $EMX_FILE \
-		--ccm $CCM_FILE \
-		--cmx $CMX_FILE \
-		--output $CMX_TXT_FILE
+		--emx ${EMX_FILE} \
+		--ccm ${CCM_FILE} \
+		--cmx ${CMX_FILE} \
+		--output ${CMX_TXT_FILE}
 fi
 
 # threshold
-if [[ $DO_THRESHOLD = 1 ]]; then
+if [[ ${DO_THRESHOLD} = 1 ]]; then
 	env time -f "%e" kinc run rmt \
-		--input $CMX_FILE \
-		--log $RMT_FILE \
-		--threads $NP
+		--input ${CMX_FILE} \
+		--log ${RMT_FILE} \
+		--threads ${NP}
 fi
 
 # extract
-if [[ $DO_EXTRACT = 1 ]]; then
+if [[ ${DO_EXTRACT} = 1 ]]; then
 	MINCORR=0
 	MAXCORR=1
-	NET_FILE="$DIRNAME/$BASENAME.th000.coexpnet.txt"
+	NET_FILE="${DIRNAME}/${BASENAME}.th000.coexpnet.txt"
 
 	env time -f "%e" kinc run extract \
-		--emx $EMX_FILE \
-		--ccm $CCM_FILE \
-		--cmx $CMX_FILE \
-		--output $NET_FILE \
-		--mincorr $MINCORR \
-		--maxcorr $MAXCORR
+		--emx ${EMX_FILE} \
+		--ccm ${CCM_FILE} \
+		--cmx ${CMX_FILE} \
+		--output ${NET_FILE} \
+		--mincorr ${MINCORR} \
+		--maxcorr ${MAXCORR}
 fi
